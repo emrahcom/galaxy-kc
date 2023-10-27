eb7b480f504106bd76174a4aa3ea4486132ee34f
emrah <emrah.eryilmaz@nordeck.net>
2023-10-27 18:20:17 +0300

    eb-app-ui, fix: add missing ;

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/config.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/config.ts
@@ -4,7 +4,7 @@ export const FORM_WIDTH = "540px";
 
 // keycloak
 export const KEYCLOAK_ORIGIN = "___KEYCLOAK_ORIGIN___";
-export const KEYCLOAK_REALM = "___KEYCLOAK_REALM___"'
+export const KEYCLOAK_REALM = "___KEYCLOAK_REALM___";
 export const KEYCLOAK_CLIENT_ID = "___KEYCLOAK_CLIENT_ID___";
 
 // this will be removed
