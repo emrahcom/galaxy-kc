e2577494cec16a59cb098aad8d6ad7e282fe8855
emrah <emrah.eryilmaz@nordeck.net>
2023-10-27 17:58:47 +0300

    eb-a-ui, refactor: keycloak params for UI

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/config.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/config.ts
@@ -1,3 +1,11 @@
+// application
 export const APP = "___GALAXY_FQDN___";
-export const KRATOS = "___KRATOS_FQDN___";
 export const FORM_WIDTH = "540px";
+
+// keycloak
+export const KEYCLOAK_ORIGIN = "___KEYCLOAK_ORIGIN___";
+export const KEYCLOAK_REALM = "___KEYCLOAK_REALM___"'
+export const KEYCLOAK_CLIENT_ID = "___KEYCLOAK_CLIENT_ID___";
+
+// this will be removed
+export const KRATOS = "___KRATOS_FQDN___";
