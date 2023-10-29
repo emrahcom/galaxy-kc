d00415bd3a6b0b2615bf1f463eea9cbfbdeb3e44
emrah <emrah.eryilmaz@nordeck.net>
2023-10-29 16:19:23 +0300

    eb-app-ui, fix: typo

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/redirect/+page.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/redirect/+page.ts
@@ -10,7 +10,7 @@ export async function load() {
   const target =
     `${KEYCLOAK_ORIGIN}/realms/${KEYCLOAK_REALM}` +
     `/protocol/openid-connect/auth?client_id=${KEYCLOAK_CLIENT_ID}` +
-    `&response_type=code&scope=openid&prompt=login}` +
+    `&response_type=code&scope=openid&prompt=login` +
     `&redirect_uri=https://${APP}/oidc/validate`;
 
   window.location.href = `${target}`;
