98fbfea1a6f7cd35d9825b01a6caca0b08aad65c
emrah <emrah.eryilmaz@nordeck.net>
2023-10-29 17:45:04 +0300

    eb-app-ui, feat: get the authorization code

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/validate/+page.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/validate/+page.ts
@@ -1,4 +1,16 @@
+import { getByCode } from "$lib/api";
+
 // -----------------------------------------------------------------------------
 export async function load() {
-  console.error("hello");
+  try {
+    const qs = new URLSearchParams(window.location.search);
+    const code = qs.get("code");
+    if (!code) throw new Error("code not found");
+
+    console.error(code);
+    const identity = getByCode("/api/adm/identity/get/bycode", code);
+
+    console.log(identity);
+  } catch {
+  }
 }
