50f6d9df13acc8fb5b9f2ac104ca6d53bfa11545
emrah <emrah.eryilmaz@nordeck.net>
2023-10-29 17:03:17 +0300

    eb-app-api, feat: initialize get identity by code

--- machines/eb-app-api/home/api/galaxy/lib/adm/identity.ts
+++ machines/eb-app-api/home/api/galaxy/lib/adm/identity.ts
@@ -1,34 +1,46 @@
 import { notFound } from "../http/response.ts";
 import { adm as wrapper } from "../http/wrapper.ts";
-import { addIdentity } from "../database/identity.ts";
-import { addProfile } from "../database/profile.ts";
+//import { addIdentity } from "../database/identity.ts";
+//import { addProfile } from "../database/profile.ts";
 
 const PRE = "/api/adm/identity";
 
 // -----------------------------------------------------------------------------
-async function add(req: Request): Promise<unknown> {
+//async function add(req: Request): Promise<unknown> {
+//  const pl = await req.json();
+//  const identityId = pl.identity_id;
+//  const email = pl.identity_email;
+//  const name = email.split("@")[0];
+//  const rows = await addIdentity(identityId);
+//
+//  if (rows[0] !== undefined) {
+//    await addProfile(
+//      identityId,
+//      name,
+//      email,
+//      true,
+//    );
+//  }
+//
+//  return rows;
+//}
+
+// -----------------------------------------------------------------------------
+async function getByCode(req: Request): Promise<unknown> {
   const pl = await req.json();
-  const identityId = pl.identity_id;
-  const email = pl.identity_email;
-  const name = email.split("@")[0];
-  const rows = await addIdentity(identityId);
+  const code = pl.code;
 
-  if (rows[0] !== undefined) {
-    await addProfile(
-      identityId,
-      name,
-      email,
-      true,
-    );
-  }
+  const identity = {
+    jwt: "my_jwt",
+  };
 
-  return rows;
+  return identity;
 }
 
 // -----------------------------------------------------------------------------
 export default async function (req: Request, path: string): Promise<Response> {
-  if (path === `${PRE}/add`) {
-    return await wrapper(add, req);
+  if (path === `${PRE}/get/bycode`) {
+    return await wrapper(getByCode, req);
   } else {
     return notFound();
   }
