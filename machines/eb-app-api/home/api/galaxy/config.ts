929f6667e8e7a856e8d7daa6a90ba7a11982a711
emrah <emrah.eryilmaz@nordeck.net>
2023-10-29 19:15:47 +0300

    eb-app-api, feat: add keycloak params into config

--- machines/eb-app-api/home/api/galaxy/config.ts
+++ machines/eb-app-api/home/api/galaxy/config.ts
@@ -7,9 +7,17 @@ export const PORT_PUBLIC = 8002;
 // database
 export const DB_NAME = Deno.env.get("DB_NAME") || "galaxy";
 export const DB_USER = Deno.env.get("DB_USER") || "galaxy";
-export const DB_PASSWD = Deno.env.get("DB_PASSWD") || "___DB_PASSWD___";
+export const DB_PASSWD = Deno.env.get("DB_PASSWD");
 export const DB_HOST = Deno.env.get("DB_HOST") || "eb-postgres";
 export const DB_PORT = Number(Deno.env.get("DB_PORT") || 5432);
 export const DB_POOL_SIZE = 8;
 export const DEFAULT_LIST_SIZE = 10;
 export const MAX_LIST_SIZE = 100;
+
+// app
+export const GALAXY_FQDN = Deno.env.get("GALAXY_FQDN");
+
+// keycloak
+export const KEYCLOAK_ORIGIN = Deno.env.get("KEYCLOAK_ORIGIN");
+export const KEYCLOAK_REALM = Deno.env.get("KEYCLOAK_REALM");
+export const KEYCLOAK_CLIENT_ID = Deno.env.get("KEYCLOAK_CLIENT_ID");
