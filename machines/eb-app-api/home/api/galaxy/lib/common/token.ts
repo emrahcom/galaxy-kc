cc71bbc4bc4dda0dd988ef45a6cdd967cee2a408
emrah <emrah.eryilmaz@nordeck.net>
2023-10-30 12:41:31 +0300

    eb-app-api, feat: token generator for API client

--- machines/eb-app-api/home/api/galaxy/lib/common/token.ts
+++ machines/eb-app-api/home/api/galaxy/lib/common/token.ts
@@ -4,6 +4,7 @@ import {
   getNumericDate,
   Payload,
 } from "https://deno.land/x/djwt/mod.ts";
+import { API_SECRET, API_TIMEOUT } from "../config.ts";
 
 // -----------------------------------------------------------------------------
 async function generateCryptoKeyHS(
@@ -24,6 +25,24 @@ async function generateCryptoKeyHS(
   );
 
   return cryptoKey;
+}
+
+// -----------------------------------------------------------------------------
+export async function generateAPIToken(userId: string): Promise<string> {
+  const alg: Algorithm = "HS256";
+  const hash = "SHA-256";
+
+  const header = { alg: alg, typ: "JWT" };
+  const cryptoKey = await generateCryptoKeyHS(API_SECRET, hash);
+  const payload: Payload = {
+    iat: getNumericDate(0),
+    exp: getNumericDate(API_TIMEOUT),
+    userId: userId,
+  };
+
+  const jwt = await create(header, payload, cryptoKey);
+
+  return jwt;
 }
 
 // -----------------------------------------------------------------------------
