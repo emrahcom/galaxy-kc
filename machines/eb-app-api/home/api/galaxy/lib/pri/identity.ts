d5cc90344ea2d90432ed1e5f0c0830b9b6535afe
emrah <emrah.eryilmaz@nordeck.net>
2023-10-30 20:40:12 +0300

    eb-app-api, feat: add getIdentityId

---
+++ machines/eb-app-api/home/api/galaxy/lib/pri/identity.ts
@@ -0,0 +1,24 @@
+import { getCookies } from "https://deno.land/std/http/cookie.ts";
+import { verify } from "https://deno.land/x/djwt/mod.ts";
+import { generateCryptoKeyHS } from "../common/token.ts";
+import { API_SECRET } from "../../config.ts";
+
+// -----------------------------------------------------------------------------
+export async function getIdentityId(req: Request): Promise<string | undefined> {
+  try {
+    const cookies = getCookies(req.headers);
+    if (!cookies) throw new Error("no cookies");
+
+    const token = cookies.token;
+    if (!token) throw new Error("no token");
+
+    const cryptoKey = await generateCryptoKeyHS(API_SECRET, "SHA-256");
+    const payload = await verify(token, cryptoKey);
+    if (!payload.userId) throw new Error("no userId");
+    if (typeof payload.userId !== "string") throw new Error("no valid userId");
+
+    return payload.userId;
+  } catch {
+    return undefined;
+  }
+}
