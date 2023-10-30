266be5ef1bf3c2b7e73726bb1c0950805b718a7e
emrah <emrah.eryilmaz@nordeck.net>
2023-10-30 15:51:00 +0300

    eb-app-ui, refactor: remove Kratos from config

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/config.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/config.ts
@@ -6,6 +6,3 @@ export const FORM_WIDTH = "540px";
 export const KEYCLOAK_ORIGIN = "___KEYCLOAK_ORIGIN___";
 export const KEYCLOAK_REALM = "___KEYCLOAK_REALM___";
 export const KEYCLOAK_CLIENT_ID = "___KEYCLOAK_CLIENT_ID___";
-
-// this will be removed
-export const KRATOS = "___KRATOS_FQDN___";
