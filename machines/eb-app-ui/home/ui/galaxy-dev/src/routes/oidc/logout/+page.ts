357cc16a4a7195b1367a5819a33908b8e56896b6
emrah <emrah.eryilmaz@nordeck.net>
2023-10-31 12:53:46 +0300

    eb-app-ui, refactor: logout from keycloak

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/logout/+page.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/logout/+page.ts
@@ -1,4 +1,19 @@
+import {
+  GALAXY_FQDN,
+  KEYCLOAK_CLIENT_ID,
+  KEYCLOAK_ORIGIN,
+  KEYCLOAK_REALM,
+} from "$lib/config";
+
+// -----------------------------------------------------------------------------
 export async function load() {
   window.sessionStorage.clear();
-  window.location.href = "/";
+  window.localStorage.clear();
+
+  const target =
+    `${KEYCLOAK_ORIGIN}/realms/${KEYCLOAK_REALM}` +
+    `/protocol/openid-connect/logout?client_id=${KEYCLOAK_CLIENT_ID}` +
+    `&post_logout_redirect_uri=https://${GALAXY_FQDN}/`;
+
+  window.location.href = `${target}`;
 }
