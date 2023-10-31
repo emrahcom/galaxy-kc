5b9641096e522983d15f72dd7fd94af2debaa41b
emrah <emrah.eryilmaz@nordeck.net>
2023-10-31 18:10:05 +0300

    eb-app-api, feat: add config endpoint

---
+++ machines/eb-app-api/home/api/galaxy/lib/adm/config.ts
@@ -0,0 +1,19 @@
+import { ok } from "../http/response.ts";
+import {
+  GALAXY_FQDN,
+  KEYCLOAK_CLIENT_ID,
+  KEYCLOAK_ORIGIN,
+  KEYCLOAK_REALM,
+} from "../../config.ts";
+
+// -----------------------------------------------------------------------------
+export default function (): Response {
+  const config = {
+    galaxy_fqdn: GALAXY_FQDN,
+    keycloak_client_id: KEYCLOAK_CLIENT_ID,
+    keycloak_origin: KEYCLOAK_ORIGIN,
+    keycloak_realm: KEYCLOAK_REALM,
+  };
+
+  return ok(JSON.stringify(config));
+}
