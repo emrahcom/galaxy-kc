07a3175c4e1666948b40af774cd4c145e3c91592
emrah <emrah.eryilmaz@nordeck.net>
2023-11-01 10:40:05 +0300

    eb-app-ui, refactor: move validate codes into the svelte page

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/validate/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { get, getByCode } from "$lib/api";
+
+  async function load() {
+    window.localStorage.clear();
+    window.sessionStorage.clear();
+    window.sessionStorage.setItem("oidc", "ok");
+
+    try {
+      await get("/api/adm/identity/clear");
+
+      const qs = new URLSearchParams(window.location.search);
+      const code = qs.get("code");
+      if (!code) throw new Error("code not found");
+
+      const identity = await getByCode("/api/adm/identity/get/bycode", code);
+      window.localStorage.setItem("username", identity.preferred_username);
+      window.localStorage.setItem("identity_sub", identity.sub);
+
+      window.location.href = "/pri";
+    } catch {
+      window.location.href = "/";
+    }
+  }
+
+  load();
+</script>
