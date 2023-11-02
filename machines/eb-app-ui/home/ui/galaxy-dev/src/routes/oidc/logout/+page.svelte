746a7421bbc2a8cfc001cfc100c58bf8c0047ee7
emrah <emrah.eryilmaz@nordeck.net>
2023-11-02 14:33:12 +0300

    eb-app-ui, refactor: logout from app, not from keycloak

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/logout/+page.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/oidc/logout/+page.svelte
@@ -2,14 +2,15 @@
   import { get } from "$lib/api";
 
   async function load() {
-    const GALAXY_FQDN = window.localStorage.getItem("galaxy_fqdn");
-    const KEYCLOAK_CLIENT_ID =
-      window.localStorage.getItem("keycloak_client_id");
-    const KEYCLOAK_ORIGIN = window.localStorage.getItem("keycloak_origin");
-    const KEYCLOAK_REALM = window.localStorage.getItem("keycloak_realm");
+    //const GALAXY_FQDN = window.localStorage.getItem("galaxy_fqdn");
+    //const KEYCLOAK_CLIENT_ID =
+    //  window.localStorage.getItem("keycloak_client_id");
+    //const KEYCLOAK_ORIGIN = window.localStorage.getItem("keycloak_origin");
+    //const KEYCLOAK_REALM = window.localStorage.getItem("keycloak_realm");
 
     window.localStorage.clear();
     window.sessionStorage.clear();
+    window.sessionStorage.setItem("oidc_checked", "ok");
 
     // Send a clearing request to API to remove the HttpOnly cookie.
     // This cookie contains the token for this session.
@@ -19,12 +20,15 @@
       // do nothing
     }
 
-    const target =
-      `${KEYCLOAK_ORIGIN}/realms/${KEYCLOAK_REALM}` +
-      `/protocol/openid-connect/logout?client_id=${KEYCLOAK_CLIENT_ID}` +
-      `&post_logout_redirect_uri=https://${GALAXY_FQDN}/`;
+    // DISABLE LOGOUT FROM KEYCLOAK, LOGOUT ONLY FROM APP
+    //const target =
+    //  `${KEYCLOAK_ORIGIN}/realms/${KEYCLOAK_REALM}` +
+    //  `/protocol/openid-connect/logout?client_id=${KEYCLOAK_CLIENT_ID}` +
+    //  `&post_logout_redirect_uri=https://${GALAXY_FQDN}/`;
 
-    window.location.replace(`${target}`);
+    //window.location.replace(`${target}`);
+
+    window.location.href = "/";
   }
 
   load();
