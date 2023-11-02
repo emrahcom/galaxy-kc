b780f5b858fc24c90a29d75554bf3c3424ac3cc0
emrah <emrah.eryilmaz@nordeck.net>
2023-11-02 12:00:51 +0300

    eb-app-ui, refactor: update authentication logic

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.ts
@@ -4,11 +4,11 @@ import { get } from "$lib/api";
 export async function load() {
   const target = window.location.pathname;
 
-  // dont continue if the target is the audience pages
-  // the audience pages dont need authentication
+  // Dont continue if the target is the audience pages.
+  // The audience pages dont need authentication.
   if (target.match("^/aud/")) return;
 
-  // get global parmeters if they are not available in the storage
+  // Get Keycloak related parameters if they are not available in the storage
   if (
     !window.localStorage.getItem("galaxy_fqdn") ||
     !window.localStorage.getItem("keycloak_client_id") ||
@@ -26,10 +26,17 @@ export async function load() {
     window.localStorage.setItem("keycloak_realm", config.keycloak_realm);
   }
 
-  // try to authenticate if the target is the entry page
-  if (target === "/" || target === "/pri") {
-    if (!window.sessionStorage.getItem("oidc_checked")) {
+  // Am I authenticated
+  const res = await post("/api/pri/hello", "{}");
+
+  window.sessionStorage.removeItem("oidc_authenticated");
+  if (res.status === 200) {
+    window.sessionStorage.setItem("oidc_checked", "ok");
+    window.sessionStorage.setItem("oidc_authenticated", "ok");
+  }
+
+  // Try to authenticate if the target is the entry page
+  if (target === "/" && !window.sessionStorage.getItem("oidc_checked")) {
       window.location.href = "/oidc/redirect-none";
-    }
   }
 }
