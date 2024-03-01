a6bca34a6dbd3ccdeb37911d7465f878439481ac
emrah <emrah.eryilmaz@nordeck.net>
2024-03-01 20:31:57 +0300

    eb-app-api, refactor: update domain-partnership SQLs

--- machines/eb-app-api/home/api/galaxy/lib/database/domain-partnership.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/domain-partnership.ts
@@ -68,6 +68,73 @@ export async function addDomainPartnershipByCode(
   };
   if (rows[0] !== undefined) await query(sql1);
 
+  // add partner to the contact list
+  const sql2 = {
+    text: `
+      INSERT INTO contact (identity_id, remote_id, name)
+      VALUES (
+        (SELECT identity_id
+         FROM domain_invite
+         WHERE code = $2
+        ),
+        $1,
+        (SELECT name
+         FROM profile
+         WHERE identity_id = $1
+           AND is_default
+        )
+      )
+      ON CONFLICT DO NOTHING`,
+    args: [
+      identityId,
+      code,
+    ],
+  };
+  if (rows[0] !== undefined) await query(sql2);
+
+  // add domain owner to the partner's contact list
+  const sql3 = {
+    text: `
+      INSERT INTO contact (identity_id, remote_id, name)
+      VALUES (
+        $1,
+        (SELECT identity_id
+         FROM domain_invite
+         WHERE code = $2
+        ),
+        (SELECT name
+         FROM profile
+         WHERE identity_id = (SELECT identity_id
+                              FROM domain_invite
+                              WHERE code = $2
+                             )
+           AND is_default
+        )
+      )
+      ON CONFLICT DO NOTHING`,
+    args: [
+      identityId,
+      code,
+    ],
+  };
+  if (rows[0] !== undefined) await query(sql3);
+
+  // remove the domain-partner candidancy if exists
+  const sql4 = {
+    text: `
+      DELETE FROM domain_partner_candidate
+      WHERE identity_id = $1
+        AND domain_id = (SELECT domain_id
+                         FROM domain_invite
+                         WHERE code = $2
+                        )`,
+    args: [
+      identityId,
+      code,
+    ],
+  };
+  if (rows[0] !== undefined) await query(sql4);
+
   return rows;
 }
 
