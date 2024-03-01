6bc5d77a2ebecf2f9db61dd71a1c8ca7d0af9999
emrah <emrah.eryilmaz@nordeck.net>
2024-03-01 23:25:01 +0300

    eb-app-ui, refactor: typo

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/profile/list.svelte
@@ -17,7 +17,7 @@
         <div class="card h-100 {p.is_default ? 'border-primary' : ''}">
           <div class="card-body text-center">
             <h5 class="card-title text-muted">{p.name}</h5>
-            <p class="card-text text-muted">{p.email ?? "-"}</p>
+            <p class="card-text text-muted">{p.email}</p>
           </div>
 
           <div class="card-footer bg-body border-0 text-center">
@@ -32,7 +32,7 @@
       </div>
     {:else}
       <Warning>
-        There is no profiles in the list. Click <Add href="/pri/profile/add" />
+        There is no profile in the list. Click <Add href="/pri/profile/add" />
         to add a new profile.
       </Warning>
     {/each}
