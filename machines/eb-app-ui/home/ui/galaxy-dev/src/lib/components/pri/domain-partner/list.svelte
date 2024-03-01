304e991be5a0515f4f29dac3eff26035c91d20b8
emrah <emrah.eryilmaz@nordeck.net>
2024-03-01 23:11:14 +0300

    eb-app-ui, refactor: contact in domain-partner list

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partner/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-partner/list.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-  import type { DomainPartner } from "$lib/types";
+  import type { DomainPartner, DomainPartnerCandidate } from "$lib/types";
   import Del from "$lib/components/common/link-del.svelte";
   import Disable from "$lib/components/common/link-disable.svelte";
   import Enable from "$lib/components/common/link-enable.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   export let partners: DomainPartner[];
+  export let candidates: DomainPartnerCandidate[];
+
+  const isEmpty = !(partners.length || candidates.length);
 </script>
 
 <!-- -------------------------------------------------------------------------->
@@ -15,10 +18,12 @@
       <div class="col-md-6 col-xl-4">
         <div class="card h-100 {p.enabled ? '' : 'border-danger'}">
           <div class="card-body text-center">
-            <h5 class="card-title text-muted">{p.profile_name}</h5>
+            <h5 class="card-title text-muted">
+              {p.contact_name || p.profile_name || ""}
+            </h5>
 
             <p class="card-text text-muted">
-              {p.profile_email}
+              {p.profile_email || ""}
             </p>
           </div>
 
@@ -33,8 +38,38 @@
           </div>
         </div>
       </div>
-    {:else}
-      <Warning>This domain has no partners.</Warning>
     {/each}
+
+    {#each candidates as c}
+      <div class="col-md-6 col-xl-4">
+        <div class="card h-100">
+          <div class="card-body text-center">
+            <h5 class="card-title text-muted">
+              {c.contact_name || c.profile_name || ""}
+            </h5>
+
+            <p class="card-text text-muted">
+              {c.profile_email || ""}
+            </p>
+
+            {#if c.status == "pending"}
+              <p class="card-text fw-bold text-success">pending</p>
+            {:else}
+              <p class="card-text fw-bold text-warning">rejected</p>
+            {/if}
+          </div>
+
+          <div class="card-footer bg-body border-0 text-center">
+            {#if c.status == "pending"}
+              <Del href="/pri/domain/partner/candidate/del/{c.id}" />
+            {/if}
+          </div>
+        </div>
+      </div>
+    {/each}
+
+    {#if isEmpty}
+      <Warning>This domain has no partner.</Warning>
+    {/if}
   </div>
 </section>
