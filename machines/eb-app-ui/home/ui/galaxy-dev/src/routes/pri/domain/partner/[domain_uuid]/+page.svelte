9b16d5c23bbd71a0b391d35b9adf6450309c55cf
emrah <emrah.eryilmaz@nordeck.net>
2024-03-01 22:22:05 +0300

    eb-app-ui, refactor: update domain-partner list route

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/partner/[domain_uuid]/+page.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/domain/partner/[domain_uuid]/+page.svelte
@@ -3,7 +3,7 @@
   import { getById, listById } from "$lib/api";
   import type { Domain } from "$lib/types";
   import List from "$lib/components/pri/domain-partner/list.svelte";
-  import Subheader from "$lib/components/common/subheader-back.svelte";
+  import Subheader from "$lib/components/common/subheader-back-add.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
   const domainId = $page.params.domain_uuid;
@@ -13,14 +13,23 @@
     domainName = item.name;
   });
   const pr2 = listById("/api/pri/domain/partner/list/bydomain", domainId, 100);
+  const pr3 = listById(
+    "/api/pri/domain/partner/candidate/list/bydomain",
+    domainId,
+    100,
+  );
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<Subheader subheader="Partners of {domainName}" hrefBack="/pri/domain" />
+<Subheader
+  subheader="Partners of {domainName}"
+  hrefBack="/pri/domain"
+  hrefAdd="/pri/domain/partner/candidate/add/{domainId}"
+/>
 
 <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
-{#await Promise.all([pr1, pr2]) then [_domain, partners]}
-  <List {partners} />
+{#await Promise.all([pr1, pr2, pr3]) then [_domain, partners, candidates]}
+  <List {partners} {candidates} />
 {:catch}
   <Warning>Something went wrong</Warning>
 {/await}
