3b30c49f0c8e07c9e6b377ab41e6851656c596d4
emrah <emrah.eryilmaz@nordeck.net>
2024-03-02 09:51:03 +0300

    eb-app-api, chore: test workflow

--- machines/eb-app-api/home/api/galaxy/config.ts
+++ machines/eb-app-api/home/api/galaxy/config.ts
@@ -4,7 +4,7 @@ export const PORT_ADMIN = 8000;
 export const PORT_PRIVATE = 8001;
 export const PORT_PUBLIC = 8002;
 
-// database
+// postgres
 export const DB_NAME = Deno.env.get("DB_NAME") || "galaxy";
 export const DB_USER = Deno.env.get("DB_USER") || "galaxy";
 export const DB_PASSWD = Deno.env.get("DB_PASSWD") || "";
@@ -14,7 +14,7 @@ export const DB_POOL_SIZE = 8;
 export const DEFAULT_LIST_SIZE = 10;
 export const MAX_LIST_SIZE = 100;
 
-// app
+// application
 export const GALAXY_FQDN = Deno.env.get("GALAXY_FQDN") || "";
 export const API_SECRET = Deno.env.get("API_SECRET") || "mysecret";
 export const API_TIMEOUT = Number(Deno.env.get("API_TIMEOUT") || 86400);
