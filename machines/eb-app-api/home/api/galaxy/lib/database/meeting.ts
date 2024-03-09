398df51448ff8c1f3375f4d6805184c7e9472a73
emrah <emrah.eryilmaz@nordeck.net>
2024-03-09 21:34:40 +0300

    fix(eb-app-api): meeting.ts

--- machines/eb-app-api/home/api/galaxy/lib/database/meeting.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/meeting.ts
@@ -70,22 +70,24 @@ export async function getMeetingLinkset(identityId: string, meetingId: string) {
     text: `
       SELECT m.id, m.name, r.name as room_name, s.name as schedule_name,
         r.has_suffix, r.suffix, d.auth_type, d.domain_attr, 'host' as join_as,
-        s.started_at, s.ended_at, s.duration,
-        extract('epoch' from age(ended_at, now()))::integer as remaining,
+        ses.started_at, ses.ended_at, ses.duration,
+        extract('epoch' from age(ses.ended_at, now()))::integer as remaining,
         pr.name as profile_name, pr.email as profile_email
       FROM meeting m
         JOIN room r ON m.room_id = r.id
+                       AND r.enabled
         JOIN domain d ON r.domain_id = d.id
+                         AND d.enabled
         JOIN identity i1 ON d.identity_id = i1.id
+                            AND i1.enabled
         JOIN identity i2 ON r.identity_id = i2.id
+                            AND i2.enabled
         LEFT JOIN meeting_schedule s ON m.id = s.meeting_id
+                                        AND s.enabled
+        LEFT JOIN meeting_session ses ON s.id = ses.meeting_schedule_id
         LEFT JOIN profile pr ON m.profile_id = pr.id
       WHERE m.id = $2
         AND m.identity_id = $1
-        AND r.enabled
-        AND d.enabled
-        AND i1.enabled
-        AND i2.enabled
         AND (r.identity_id = $1
              OR EXISTS (SELECT 1
                         FROM room_partner
@@ -104,9 +106,9 @@ export async function getMeetingLinkset(identityId: string, meetingId: string) {
                        )
             )
         AND (m.schedule_type != 'scheduled'
-             OR s.ended_at > now()
+             OR ses.ended_at > now()
             )
-      ORDER BY started_at
+      ORDER BY ses.started_at
       LIMIT 1
         `,
     args: [
@@ -134,17 +136,25 @@ export async function getMeetingLinksetByMembership(
     text: `
       SELECT m.id, m.name, r.name as room_name, s.name as schedule_name,
         r.has_suffix, r.suffix, d.auth_type, d.domain_attr, mem.join_as,
-        s.started_at, s.ended_at, s.duration,
-        extract('epoch' from age(ended_at, now()))::integer as remaining,
+        ses.started_at, ses.ended_at, ses.duration,
+        extract('epoch' from age(ses.ended_at, now()))::integer as remaining,
         pr.name as profile_name, pr.email as profile_email
       FROM meeting_member mem
         JOIN meeting m ON mem.meeting_id = m.id
+                          AND m.enabled
         JOIN room r ON m.room_id = r.id
+                       AND r.enabled
         JOIN domain d ON r.domain_id = d.id
+                         AND d.enabled
         JOIN identity i1 ON d.identity_id = i1.id
+                            AND i1.enabled
         JOIN identity i2 ON r.identity_id = i2.id
+                            AND i2.enabled
         JOIN identity i3 ON m.identity_id = i3.id
+                            AND i3.enabled
         LEFT JOIN meeting_schedule s ON m.id = s.meeting_id
+                                        AND s.enabled
+        LEFT JOIN meeting_session ses ON s.id = ses.meeting_schedule_id
         LEFT JOIN profile pr ON mem.profile_id = pr.id
       WHERE mem.id = $2
         AND mem.identity_id = $1
@@ -152,17 +162,11 @@ export async function getMeetingLinksetByMembership(
         AND CASE mem.join_as
               WHEN 'host' THEN true
               ELSE (m.schedule_type != 'scheduled'
-                    OR (s.started_at - interval '1 min' < now()
-                        AND s.ended_at > now()
+                    OR (ses.started_at - interval '1 min' < now()
+                        AND ses.ended_at > now()
                        )
                    )
             END
-        AND m.enabled
-        AND r.enabled
-        AND d.enabled
-        AND i1.enabled
-        AND i2.enabled
-        AND i3.enabled
         AND (r.identity_id = m.identity_id
              OR EXISTS (SELECT 1
                         FROM room_partner
@@ -181,9 +185,9 @@ export async function getMeetingLinksetByMembership(
                        )
             )
         AND (m.schedule_type != 'scheduled'
-             OR s.ended_at > now()
+             OR ses.ended_at > now()
             )
-      ORDER BY started_at
+      ORDER BY ses.started_at
       LIMIT 1
         `,
     args: [
@@ -208,17 +212,25 @@ export async function getMeetingLinksetByCode(code: string) {
     text: `
       SELECT m.id, m.name, r.name as room_name, s.name as schedule_name,
         r.has_suffix, r.suffix, d.auth_type, d.domain_attr, iv.join_as,
-        s.started_at, s.ended_at, s.duration,
-        extract('epoch' from age(ended_at, now()))::integer as remaining,
+        ses.started_at, ses.ended_at, ses.duration,
+        extract('epoch' from age(ses.ended_at, now()))::integer as remaining,
         '' as profile_name, '' as profile_email
       FROM meeting_invite iv
         JOIN meeting m ON iv.meeting_id = m.id
+                          AND m.enabled
         JOIN room r ON m.room_id = r.id
+                       AND r.enabled
         JOIN domain d ON r.domain_id = d.id
+                         AND d.enabled
         JOIN identity i1 ON d.identity_id = i1.id
+                            AND i1.enabled
         JOIN identity i2 ON r.identity_id = i2.id
+                            AND i2.enabled
         JOIN identity i3 ON m.identity_id = i3.id
+                            AND i3.enabled
         LEFT JOIN meeting_schedule s ON m.id = s.meeting_id
+                                        AND s.enabled
+        LEFT JOIN meeting_session ses ON s.id = ses.meeting_schedule_id
       WHERE iv.code = $1
         AND iv.enabled
         AND iv.invite_to = 'audience'
@@ -226,17 +238,11 @@ export async function getMeetingLinksetByCode(code: string) {
         AND CASE iv.join_as
               WHEN 'host' THEN true
               ELSE (m.schedule_type != 'scheduled'
-                    OR (s.started_at - interval '1 min' < now()
-                        AND s.ended_at > now()
+                    OR (ses.started_at - interval '1 min' < now()
+                        AND ses.ended_at > now()
                        )
                    )
             END
-        AND m.enabled
-        AND r.enabled
-        AND d.enabled
-        AND i1.enabled
-        AND i2.enabled
-        AND i3.enabled
         AND (r.identity_id = m.identity_id
              OR EXISTS (SELECT 1
                         FROM room_partner
@@ -255,9 +261,9 @@ export async function getMeetingLinksetByCode(code: string) {
                        )
             )
         AND (m.schedule_type != 'scheduled'
-             OR s.ended_at > now()
+             OR ses.ended_at > now()
             )
-      ORDER BY s.started_at
+      ORDER BY ses.started_at
       LIMIT 1`,
     args: [
       code,
@@ -289,24 +295,34 @@ export async function listMeeting(
         r.name as room_name, m.schedule_type,
         (CASE m.schedule_type
            WHEN 'scheduled' THEN array(SELECT started_at
-                                       FROM meeting_schedule
-                                       WHERE meeting_id = m.id
+                                       FROM meeting_session
+                                       WHERE meeting_schedule_id IN (
+                                           SELECT id
+                                           FROM meeting_schedule
+                                           WHERE meeting_id = m.id
+                                             AND enabled
+                                         )
                                          AND ended_at > now()
                                        ORDER BY started_at
                                       )
            ELSE array[]::timestamp with time zone[]
          END
-        ) as schedule_list,
+        ) as session_list,
         (CASE m.schedule_type
            WHEN 'scheduled' THEN (SELECT min(started_at)
-                                  FROM meeting_schedule
-                                  WHERE meeting_id = m.id
+                                  FROM meeting_session
+                                  WHERE meeting_schedule_id IN (
+                                      SELECT id
+                                      FROM meeting_schedule
+                                      WHERE meeting_id = m.id
+                                        AND enabled
+                                    )
                                     AND ended_at > now()
                                  )
            WHEN 'ephemeral' THEN CURRENT_DATE
            ELSE CURRENT_DATE + 1
          END
-        ) as scheduled_at,
+        ) as session_at,
         m.hidden, m.restricted, m.subscribable, m.enabled,
         (r.enabled AND i2.enabled
          AND d.enabled AND i1.enabled
@@ -347,24 +363,34 @@ export async function listMeeting(
         '' as room_name, m.schedule_type,
         (CASE m.schedule_type
            WHEN 'scheduled' THEN array(SELECT started_at
-                                       FROM meeting_schedule
-                                       WHERE meeting_id = m.id
+                                       FROM meeting_session
+                                       WHERE meeting_schedule_id IN (
+                                           SELECT id
+                                           FROM meeting_schedule
+                                           WHERE meeting_id = m.id
+                                             AND enabled
+                                         )
                                          AND ended_at > now()
                                        ORDER BY started_at
                                       )
            ELSE array[]::timestamp with time zone[]
          END
-        ) as schedule_list,
+        ) as session_list,
         (CASE m.schedule_type
            WHEN 'scheduled' THEN (SELECT min(started_at)
-                                  FROM meeting_schedule
-                                  WHERE meeting_id = m.id
+                                  FROM meeting_session
+                                  WHERE meeting_schedule_id IN (
+                                      SELECT id
+                                      FROM meeting_schedule
+                                      WHERE meeting_id = m.id
+                                        AND enabled
+                                    )
                                     AND ended_at > now()
                                  )
            WHEN 'ephemeral' THEN CURRENT_DATE
            ELSE CURRENT_DATE + 1
          END
-        ) as scheduled_at,
+        ) as session_at,
         m.hidden, m.restricted, m.subscribable, m.enabled,
         (mem.enabled AND i3.enabled
          AND r.enabled AND i2.enabled
@@ -399,7 +425,7 @@ export async function listMeeting(
         JOIN identity i3 ON m.identity_id = i3.id
       WHERE mem.identity_id = $1
 
-      ORDER BY scheduled_at, name
+      ORDER BY session_at, name
       LIMIT $2 OFFSET $3`,
     args: [
       identityId,
@@ -427,17 +453,17 @@ export async function listPublicMeeting(
         m.subscribable
       FROM meeting m
         JOIN room r ON m.room_id = r.id
+                       AND r.enabled
         JOIN domain d ON r.domain_id = d.id
+                         AND d.enabled
         JOIN identity i1 ON d.identity_id = i1.id
+                            AND i1.enabled
         JOIN identity i2 ON r.identity_id = i2.id
+                            AND i2.enabled
         JOIN identity i3 ON m.identity_id = i3.id
+                            AND i3.enabled
       WHERE NOT m.hidden
         AND m.enabled
-        AND r.enabled
-        AND d.enabled
-        AND i1.enabled
-        AND i2.enabled
-        AND i3.enabled
       ORDER BY m.created_at DESC
       LIMIT $1 OFFSET $2`,
     args: [
