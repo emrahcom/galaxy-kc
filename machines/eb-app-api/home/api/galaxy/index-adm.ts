cfec488b84071bff56de5a936b69a38365b5ef61
emrah <emrah.eryilmaz@nordeck.net>
2024-03-09 20:54:04 +0300

    feat(eb-app-api): migration and cronjob into index-adm

--- machines/eb-app-api/home/api/galaxy/index-adm.ts
+++ machines/eb-app-api/home/api/galaxy/index-adm.ts
@@ -1,11 +1,35 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 import { HOSTNAME, PORT_ADMIN } from "./config.ts";
 import { methodNotAllowed, notFound } from "./lib/http/response.ts";
+import migrate from "./lib/adm/migration.ts";
+import doit from "./lib/adm/cronjob.ts";
 import hello from "./lib/adm/hello.ts";
 import config from "./lib/adm/config.ts";
 import identity from "./lib/adm/identity.ts";
 
 const PRE = "/api/adm";
+
+// -----------------------------------------------------------------------------
+async function migration() {
+  try {
+    await migrate();
+
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
+// -----------------------------------------------------------------------------
+async function cronjob() {
+  try {
+    await doit();
+  } finally {
+    // rerun in 10 min
+    setTimeout(cronjob, 10 * 60 * 1000);
+  }
+}
 
 // -----------------------------------------------------------------------------
 async function route(req: Request, path: string): Promise<Response> {
@@ -34,7 +58,15 @@ async function handler(req: Request): Promise<Response> {
 }
 
 // -----------------------------------------------------------------------------
-function main() {
+async function main() {
+  // migrate the database before starting if needed
+  const isMigrated = await migration();
+  if (!isMigrated) Deno.exit(1);
+
+  // start the cronjob thread
+  cronjob();
+
+  // start API
   serve(handler, {
     hostname: HOSTNAME,
     port: PORT_ADMIN,
