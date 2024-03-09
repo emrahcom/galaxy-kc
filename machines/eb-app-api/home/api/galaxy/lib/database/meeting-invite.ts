51d9ed61b6767b3e971f2b1ccae7109894e5c6e2
emrah <emrah.eryilmaz@nordeck.net>
2024-03-09 21:29:25 +0300

    fix(eb-app-api): meeting-invite.ts

--- machines/eb-app-api/home/api/galaxy/lib/database/meeting-invite.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/meeting-invite.ts
@@ -30,11 +30,16 @@ export async function getMeetingInviteByCode(code: string) {
       SELECT m.name as meeting_name, m.info as meeting_info, iv.code,
         iv.invite_to, m.schedule_type,
         array(SELECT array[started_at, ended_at]
-              FROM meeting_schedule
-              WHERE meeting_id = m.id
+              FROM meeting_session
+              WHERE meeting_schedule_id IN (SELECT id
+                                            FROM meeting_schedule
+                                            WHERE meeting_id = m.id
+                                              AND enabled
+                                           )
                 AND ended_at > now()
+              ORDER BY started_at
               LIMIT 8
-             ) as schedule_list
+             ) as session_list
       FROM meeting_invite iv
         JOIN meeting m ON iv.meeting_id = m.id
       WHERE iv.code = $1
