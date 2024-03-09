655bfb820715cb9636da9371ad4c87271e993c3b
emrah <emrah.eryilmaz@nordeck.net>
2024-03-09 21:21:39 +0300

    feat(eb-app-api): meeting-session.ts

---
+++ machines/eb-app-api/home/api/galaxy/lib/database/meeting-session.ts
@@ -0,0 +1,61 @@
+import { fetch } from "./common.ts";
+import type { Attr, Id } from "./types.ts";
+
+// -----------------------------------------------------------------------------
+function checkScheduleAttrOnce(scheduleAttr: Attr) {
+  if (Number(scheduleAttr.once_duration) < 1) {
+    new Error("duration is out of range");
+  }
+
+  if (Number(scheduleAttr.once_duration) > 1440) {
+    throw new Error("duration is out of range");
+  }
+}
+
+// -----------------------------------------------------------------------------
+export function checkScheduleAttr(scheduleAttr: Attr) {
+  if (scheduleAttr.type === "once") {
+    checkScheduleAttrOnce(scheduleAttr);
+  } else {
+    throw new Error("Unknow schedule type");
+  }
+}
+
+// -----------------------------------------------------------------------------
+export async function addMeetingSessionOnce(
+  meetingScheduleId: string,
+  scheduleAttr: Attr,
+) {
+  const sql = {
+    text: `
+      INSERT INTO meeting_session (meeting_schedule_id, started_at, duration,
+        ended_at)
+      VALUES ($1, $2, $3,
+        $2::timestamptz + $3::integer * interval '1 min')
+      RETURNING id, created_at as at`,
+    args: [
+      meetingScheduleId,
+      scheduleAttr.once_started_at,
+      scheduleAttr.once_duration,
+    ],
+  };
+
+  return await fetch(sql) as Id[];
+}
+
+// -----------------------------------------------------------------------------
+export async function delMeetingSessionBySchedule(
+  meetingScheduleId: string,
+) {
+  const sql = {
+    text: `
+      DELETE FROM meeting_session
+      WHERE meeting_schedule_id = $1
+      RETURNING id, created_at as at`,
+    args: [
+      meetingScheduleId,
+    ],
+  };
+
+  return await fetch(sql) as Id[];
+}
