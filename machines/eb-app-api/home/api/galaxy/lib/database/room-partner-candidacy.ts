0c93f313de0dae1d88b8abe6c3e1a31d451beb2f
emrah <emrah.eryilmaz@nordeck.net>
2024-03-25 16:44:57 +0300

    refactor(eb-app-api): transaction in room-partner-candidacy

--- machines/eb-app-api/home/api/galaxy/lib/database/room-partner-candidacy.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/room-partner-candidacy.ts
@@ -1,4 +1,4 @@
-import { fetch, query } from "./common.ts";
+import { fetch, pool } from "./common.ts";
 import type { Id, RoomPartnerCandidacy } from "./types.ts";
 
 // -----------------------------------------------------------------------------
@@ -65,6 +65,10 @@ export async function acceptRoomPartnerCandidacy(
   identityId: string,
   candidacyId: string,
 ) {
+  using client = await pool.connect();
+  const trans = client.createTransaction("transaction");
+  await trans.begin();
+
   const sql = {
     text: `
       INSERT INTO room_partner (identity_id, room_id)
@@ -82,7 +86,7 @@ export async function acceptRoomPartnerCandidacy(
       candidacyId,
     ],
   };
-  const rows = await fetch(sql) as Id[];
+  const { rows: rows } = await trans.queryObject(sql);
 
   // add partner to the contact list if not exists
   const sql1 = {
@@ -110,7 +114,7 @@ export async function acceptRoomPartnerCandidacy(
       candidacyId,
     ],
   };
-  if (rows[0] !== undefined) await query(sql1);
+  await trans.queryObject(sql1);
 
   // add room owner to the partner's contact list if not exists
   const sql2 = {
@@ -145,7 +149,7 @@ export async function acceptRoomPartnerCandidacy(
       candidacyId,
     ],
   };
-  if (rows[0] !== undefined) await query(sql2);
+  await trans.queryObject(sql2);
 
   // remove the room-partner candidancy if the add action is successful
   const sql3 = {
@@ -158,9 +162,11 @@ export async function acceptRoomPartnerCandidacy(
       candidacyId,
     ],
   };
-  if (rows[0] !== undefined) await query(sql3);
+  await trans.queryObject(sql3);
 
-  return rows;
+  await trans.commit();
+
+  return rows as Id[];
 }
 
 // -----------------------------------------------------------------------------
