616708f179f6eade5d25df5ec38d35c1498ba73b
emrah <emrah.eryilmaz@nordeck.net>
2024-04-09 15:04:30 +0300

    feat(eb-app-ui): add form-select-number component

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/form-select-number.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let disabled = false;
+  export let id: string;
+  export let label: string;
+  export let max = 100;
+  export let min = 1;
+  export let required = true;
+  export let unit: string;
+  export let value: number;
+
+  const options = Array.from(
+    { length: max - min + 1 },
+    (_, index) => min + index,
+  );
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="form-floating my-3">
+  <select
+    class="form-select"
+    {id}
+    bind:value
+    aria-label={label}
+    {disabled}
+    {required}
+  >
+    {#each options as opt}
+      {#if unit}
+        <option value={opt}>{opt} {unit}{opt > 1 ? "s" : ""}</option>
+      {:else}
+        <option value={opt}>{opt}</option>
+      {/if}
+    {/each}
+  </select>
+  <label for={id}>{label}</label>
+</div>
