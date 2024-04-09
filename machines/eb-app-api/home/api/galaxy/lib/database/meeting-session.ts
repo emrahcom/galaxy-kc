4c049d5097abbae23542b185e2637afe099923cc
emrah <emrah.eryilmaz@nordeck.net>
2024-04-09 14:56:20 +0300

    refactor(eb-app-api): add addMeetingSessionDaily function

--- machines/eb-app-api/home/api/galaxy/lib/database/meeting-session.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/meeting-session.ts
@@ -6,7 +6,6 @@ function checkScheduleAttrOnce(scheduleAttr: Attr) {
   if (Number(scheduleAttr.duration) < 1) {
     throw new Error("duration is out of range");
   }
-
   if (Number(scheduleAttr.duration) > 1440) {
     throw new Error("duration is out of range");
   }
@@ -17,9 +16,23 @@ function checkScheduleAttrDaily(scheduleAttr: Attr) {
   if (Number(scheduleAttr.duration) < 1) {
     throw new Error("duration is out of range");
   }
-
   if (Number(scheduleAttr.duration) > 1440) {
     throw new Error("duration is out of range");
+  }
+  if (scheduleAttr.rep_end_type !== "x") {
+    throw new Error("wrong rep_end_type");
+  }
+  if (Number(scheduleAttr.rep_end_x) < 1) {
+    throw new Error("times is out of range");
+  }
+  if (Number(scheduleAttr.rep_end_x) > 99) {
+    throw new Error("times is out of range");
+  }
+  if (Number(scheduleAttr.rep_every) < 1) {
+    throw new Error("rep_every is out of range");
+  }
+  if (Number(scheduleAttr.rep_every) > 30) {
+    throw new Error("rep_every is out of range");
   }
 }
 
@@ -28,7 +41,6 @@ function checkScheduleAttrWeekly(scheduleAttr: Attr) {
   if (Number(scheduleAttr.duration) < 1) {
     throw new Error("duration is out of range");
   }
-
   if (Number(scheduleAttr.duration) > 1440) {
     throw new Error("duration is out of range");
   }
@@ -80,10 +92,35 @@ async function addMeetingSessionDaily(
   meetingScheduleId: string,
   scheduleAttr: Attr,
 ) {
-  // not implemented yet
-  await console.log(trans);
-  await console.log(meetingScheduleId);
-  await console.log(scheduleAttr);
+  const now = new Date();
+  const started_at = new Date(scheduleAttr.started_at);
+
+  for (let i = 0; i < Number(scheduleAttr.rep_end_x); i++) {
+    const session_start = started_at.getTime() +
+      i * Number(scheduleAttr.rep_every) * 24 * 60 * 60 * 1000;
+    const session_end = session_start +
+      Number(scheduleAttr.duration) * 60 * 1000;
+
+    // if this session is already over, skip it
+    if (now.getTime() > session_end) continue;
+
+    const at = new Date(session_start);
+    const sql = {
+      text: `
+        INSERT INTO meeting_session (meeting_schedule_id, started_at, duration,
+          ended_at)
+        VALUES ($1, $2, $3,
+          $2::timestamptz + $3::integer * interval '1 min')
+        RETURNING id, created_at as at`,
+      args: [
+        meetingScheduleId,
+        at.toISOString(),
+        scheduleAttr.duration,
+      ],
+    };
+
+    await trans.queryObject(sql);
+  }
 }
 
 // -----------------------------------------------------------------------------
