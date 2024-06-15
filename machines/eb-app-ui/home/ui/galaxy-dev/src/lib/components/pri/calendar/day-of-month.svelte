d263e22564f4e39f0eb3f45606e1f06d0f72157b
emrah <emrah.eryilmaz@nordeck.net>
2024-06-15 21:57:43 +0300

    refactor(eb-app-ui): calendar

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/calendar/day-of-month.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import {
+    getCalendarDay,
+    toLocaleDate,
+    toLocaleTime,
+    toLocaleMonthName,
+  } from "$lib/common";
+  import type { MeetingSchedule222 } from "$lib/types";
+
+  export let calendarDay: string;
+  export let today: string;
+  export let firstDay: string;
+  export let week: number;
+  export let day: number;
+  export let calendar: MeetingSchedule222[];
+
+  const focusedDay = getCalendarDay(firstDay, week, day);
+  const dayOfMonth = Number(focusedDay.slice(-2));
+  const meetings = calendar.filter(
+    (m) => focusedDay === toLocaleDate(m.started_at),
+  );
+
+  let month = "";
+  if (dayOfMonth === 1) month = toLocaleMonthName(focusedDay);
+
+  let bgColor = "";
+  if (today === focusedDay) {
+    bgColor = "bg-primary-subtle";
+  } else if (focusedDay < today) {
+    bgColor = "bg-light";
+  }
+
+  // ---------------------------------------------------------------------------
+  function join(m: MeetingSchedule222) {
+    if (m.membership_id) {
+      window.location.href = `/pri/member/waiting/${m.membership_id}#${calendarDay}`;
+    } else {
+      window.location.href = `/pri/owner/waiting/${m.meeting_id}#${calendarDay}`;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<div class="col h-100 p-0 overflow-y-auto {bgColor}">
+  <div class="row mx-1">{month} {dayOfMonth}</div>
+  <div class="d-grid">
+    {#each meetings as m}
+      <button
+        type="button"
+        class="btn btn-sm btn-primary p-0 m-1 text-start text-nowrap overflow-x-hidden"
+        on:click={() => join(m)}
+      >
+        {toLocaleTime(m.started_at)}
+        {m.meeting_name}
+      </button>
+    {/each}
+  </div>
+</div>
---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/calendar/list.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import {
+    getFirstDayOfMonth,
+    getFirstDayOfWeek,
+    getToday,
+    toLocaleDate,
+  } from "$lib/common";
+  import Day from "$lib/components/pri/calendar/day-of-month.svelte";
+  import type { MeetingSchedule222 } from "$lib/types";
+
+  export let date: string;
+  export let calendar: MeetingSchedule222[];
+
+  const DAYS = [0, 1, 2, 3, 4, 5, 6];
+  const WEEKS = [0, 1, 2, 3, 4, 5];
+
+  const calendarDay = toLocaleDate(date);
+  const firstOfMonth = getFirstDayOfMonth(date);
+  const firstDay = getFirstDayOfWeek(firstOfMonth);
+  const today = getToday();
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="list" class="h-100">
+  <div class="row mx-auto mt-2" style="height:98%">
+    <table class="table table-bordered h-100" style="table-layout: fixed">
+      <thead>
+        <tr>
+          <th scope="col">Sun</th>
+          <th scope="col">Mon</th>
+          <th scope="col">Tue</th>
+          <th scope="col">Wed</th>
+          <th scope="col">Thu</th>
+          <th scope="col">Fri</th>
+          <th scope="col">Sat</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each WEEKS as week}
+          <tr style="height:86px">
+            {#each DAYS as day}
+              <td class="p-0">
+                <Day {calendarDay} {today} {firstDay} {week} {day} {calendar} />
+              </td>
+            {/each}
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+</section>
