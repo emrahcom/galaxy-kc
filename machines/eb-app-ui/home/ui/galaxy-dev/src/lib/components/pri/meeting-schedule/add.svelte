7eb7880991c3d22ca5ab1c26d97b5b1f8f12dcdd
emrah <emrah.eryilmaz@nordeck.net>
2024-06-15 21:56:11 +0300

    refactor(eb-app-ui): meeting-schedule/add.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/add.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-schedule/add.svelte
@@ -7,9 +7,9 @@
     dateAfterXDays,
     getDuration,
     getEndTime,
+    getLastDayOfWeek,
+    getToday,
     isOver,
-    lastDayOfWeek,
-    today,
   } from "$lib/common";
   import type { Meeting } from "$lib/types";
   import Cancel from "$lib/components/common/button-cancel.svelte";
@@ -28,12 +28,12 @@
   export let meeting: Meeting;
   const hash = $page.url.hash;
 
-  const notBefore = today();
+  const notBefore = getToday();
   const defaultDuration = 30;
   let duration = defaultDuration;
-  let date0 = today();
+  let date0 = getToday();
   const dateAfter90Days = dateAfterXDays(90);
-  let date1 = lastDayOfWeek(`${dateAfter90Days}T00:00:00`);
+  let date1 = getLastDayOfWeek(`${dateAfter90Days}T00:00:00`);
   let time0 = "08:30";
   let time1 = getEndTime(time0, defaultDuration);
   let allDay = false;
