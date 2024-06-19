d2e4b21cbd0d7909de455a66806766734f84e683
emrah <emrah.eryilmaz@nordeck.net>
2024-06-19 12:54:04 +0300

    refactor(eb-ap-api): upgrade deno http cookie

--- machines/eb-app-api/home/api/galaxy/lib/pri/identity.ts
+++ machines/eb-app-api/home/api/galaxy/lib/pri/identity.ts
@@ -1,4 +1,4 @@
-import { getCookies } from "https://deno.land/std@0.220.1/http/cookie.ts";
+import { getCookies } from "https://deno.land/std@0.224.0/http/cookie.ts";
 import { verify } from "https://deno.land/x/djwt@v3.0.2/mod.ts";
 import { generateCryptoKeyHS } from "../common/token.ts";
 import { API_SECRET } from "../../config.ts";
