8816d67a472297ac344bb060b8e0a5afd6b5ff3f
emrah <emrah.eryilmaz@nordeck.net>
2024-06-23 13:36:30 +0300

    refactor(eb-app-ui): import bootstrp in layout

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import "bootstrap-icons/font/bootstrap-icons.css";
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import "bootstrap/dist/js/bootstrap.bundle.min.js";
   import NavBarPri from "$lib/components/nav/bar-pri.svelte";
   import NavBarPub from "$lib/components/nav/bar-pub-kc.svelte";
 
