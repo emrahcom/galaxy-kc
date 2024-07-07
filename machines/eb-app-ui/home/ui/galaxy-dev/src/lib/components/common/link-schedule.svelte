55d3fa2785a83649780dc841d20c35526cb953b8
emrah <emrah.eryilmaz@nordeck.net>
2024-07-07 09:18:01 +0300

    refactor(eb-app-ui): use template for schedule button

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-schedule.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/link-schedule.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import LinkButton from "$lib/components/common/link-button-template.svelte";
+
   export let href: string;
+  export let title = "Show schedules";
+
+  const icon = "bi-clock";
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<a {href} class="btn btn-outline-dark btn-sm" role="button">
-  <i class="bi bi-clock" />
-</a>
+<LinkButton {href} {icon} {title} />
