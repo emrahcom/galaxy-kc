89779ef121bf711cc86e835f3f5aa9fc935b27a3
emrah <emrah.eryilmaz@nordeck.net>
2024-10-14 16:21:54 +0300

    refactor(eb-app-api): sync identity.ts

--- machines/eb-app-api/home/api/galaxy/lib/pri/identity.ts
+++ machines/eb-app-api/home/api/galaxy/lib/pri/identity.ts
@@ -1,7 +1,12 @@
 import { getCookies } from "jsr:@std/http@1.0.5/cookie";
 import { verify } from "jsr:@emrahcom/jwt";
+import { notFound } from "../http/response.ts";
+import { pri as wrapper } from "../http/wrapper.ts";
+import { updatePresence } from "../database/identity.ts";
 import { generateCryptoKeyHS } from "../common/token.ts";
 import { API_SECRET } from "../../config.ts";
+
+const PRE = "/api/pri/identity";
 
 // -----------------------------------------------------------------------------
 export async function getIdentityId(req: Request): Promise<string | undefined> {
@@ -20,5 +25,23 @@ export async function getIdentityId(req: Request): Promise<string | undefined> {
     return payload.userId;
   } catch {
     return undefined;
+  }
+}
+
+// -----------------------------------------------------------------------------
+async function ping(_req: Request, identityId: string): Promise<unknown> {
+  return await updatePresence(identityId);
+}
+
+// -----------------------------------------------------------------------------
+export default async function (
+  req: Request,
+  path: string,
+  identityId: string,
+): Promise<Response> {
+  if (path === `${PRE}/ping`) {
+    return await wrapper(ping, req, identityId);
+  } else {
+    return notFound();
   }
 }
