2cd7d19606c3b49595210644c3747c17cf09aef2
emrah <emrah.eryilmaz@nordeck.net>
2024-10-14 16:51:55 +0300

    refactor(eb-app-api): sync intercom.ts

---
+++ machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
@@ -0,0 +1,89 @@
+import { fetch } from "./common.ts";
+import type { Id, IntercomMessage, IntercomStatus } from "./types.ts";
+
+// -----------------------------------------------------------------------------
+export async function getIntercom(identityId: string, intercomId: string) {
+  const sql = {
+    text: `
+      SELECT ic.id, co.id as contact_id, co.name as contact_name, status,
+        message_type, intercom_attr, expired_at
+      FROM intercom ic
+        JOIN contact co ON co.identity_id = $1
+                            AND co.remote_id = ic.identity_id
+      WHERE ic.id = $2
+        AND ic.remote_id = $1`,
+    args: [
+      identityId,
+      intercomId,
+    ],
+  };
+
+  return await fetch(sql) as IntercomMessage[];
+}
+
+// -----------------------------------------------------------------------------
+export async function listIntercom(
+  identityId: string,
+  limit: number,
+  offset: number,
+) {
+  const sql = {
+    text: `
+      SELECT ic.id, co.id as contact_id, co.name as contact_name, status,
+        message_type, intercom_attr, expired_at
+      FROM intercom ic
+        JOIN contact co ON co.identity_id = $1
+                           AND co.remote_id = ic.identity_id
+      WHERE ic.remote_id = $1
+        AND expired_at > now()
+        AND status = 'none'
+      LIMIT $2 OFFSET $3`,
+    args: [
+      identityId,
+      limit,
+      offset,
+    ],
+  };
+
+  return await fetch(sql) as IntercomMessage[];
+}
+
+// -----------------------------------------------------------------------------
+export async function delIntercom(identityId: string, intercomId: string) {
+  const sql = {
+    text: `
+      DELETE FROM intercom
+      WHERE id = $2
+        AND identity_id = $1
+      RETURNING id, now() as at`,
+    args: [
+      identityId,
+      intercomId,
+    ],
+  };
+
+  return await fetch(sql) as Id[];
+}
+
+// -----------------------------------------------------------------------------
+export async function setStatusIntercom(
+  identityId: string,
+  intercomId: string,
+  status: IntercomStatus,
+) {
+  const sql = {
+    text: `
+      UPDATE intercom
+      SET status = $3
+      WHERE id = $2
+        AND remote_id = $1
+      RETURNING id, now() as at`,
+    args: [
+      identityId,
+      intercomId,
+      status,
+    ],
+  };
+
+  return await fetch(sql) as Id[];
+}
