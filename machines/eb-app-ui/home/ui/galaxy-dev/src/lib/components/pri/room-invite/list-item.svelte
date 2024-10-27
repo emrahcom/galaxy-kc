1054861f2ac5b4a6bea138db11565f9a60cfeddf
emrah <emrah.eryilmaz@nordeck.net>
2024-10-27 19:20:03 +0300

    feat(eb-app-ui): sync for Svelte5, room-invite/list-item.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-invite/list-item.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/room-invite/list-item.svelte
@@ -7,7 +7,11 @@
   import Disable from "$lib/components/common/link-disable.svelte";
   import Enable from "$lib/components/common/link-enable.svelte";
 
-  export let p: RoomInvite;
+  interface Props {
+    p: RoomInvite;
+  }
+
+  let { p }: Props = $props();
 
   // ---------------------------------------------------------------------------
   function copy(code: string) {
@@ -32,7 +36,7 @@
       </p>
 
       {#if p.enabled}
-        <Copy label="copy" on:click={() => copy(p.code)} />
+        <Copy label="copy" onclick={() => copy(p.code)} />
       {/if}
     </div>
 
