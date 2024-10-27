eb890845044ca4d845f527a884781409248c6cb7
emrah <emrah.eryilmaz@nordeck.net>
2024-10-27 18:17:09 +0300

    feat(eb-app-ui): sync for Svelte5, message/list.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/message/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/message/list.svelte
@@ -2,14 +2,18 @@
   import type { IntercomMessage } from "$lib/types";
   import Call from "$lib/components/message/call.svelte";
 
-  export let messages: IntercomMessage[];
+  interface Props {
+    messages: IntercomMessage[];
+  }
+
+  let { messages }: Props = $props();
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <div id="messages" class="toast-container bottom-0 end-0 p-3">
   {#each messages as msg}
     {#if msg.message_type === "call"}
-      <Call bind:msg />
+      <Call {msg} />
     {/if}
   {/each}
 </div>
