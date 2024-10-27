9b8e9c9d63e2e1d231afd0403a27fd44d9f3d263
emrah <emrah.eryilmaz@nordeck.net>
2024-10-27 18:07:37 +0300

    feat(eb-app-ui): sync for Svelte5, common/button-cancel.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-cancel.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-cancel.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-  export let label = "Cancel";
-  export let disabled = false;
+  interface Props {
+    disabled?: boolean;
+    label?: string;
+    onclick: (e: MouseEvent) => void;
+  }
+
+  let { disabled = false, label = "Cancel", onclick }: Props = $props();
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<button class="btn btn-outline-secondary" type="button" {disabled} on:click>
+<button class="btn btn-outline-secondary" type="button" {disabled} {onclick}>
   {label}
 </button>
