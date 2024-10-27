f1c61c9febe847d235a38d1facda7928c5f760b9
emrah <emrah.eryilmaz@nordeck.net>
2024-10-27 18:07:57 +0300

    feat(eb-app-ui): sync for Svelte5, common/button-copy.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-copy.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/button-copy.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-  export let label = "copy";
+  interface Props {
+    label?: string;
+    onclick: (e: MouseEvent) => void;
+  }
+
+  let { label = "copy", onclick }: Props = $props();
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<button class="btn btn-sm btn-outline-primary" type="button" on:click>
+<button class="btn btn-sm btn-outline-primary" type="button" {onclick}>
   {label}
 </button>
