297d1e5d92884e408a36e38847d2748633b4aae5
emrah <emrah.eryilmaz@nordeck.net>
2024-10-27 19:06:15 +0300

    feat(eb-app-ui): sync for Svelte5, meeting-invite/disable.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-invite/disable.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/meeting-invite/disable.svelte
@@ -14,10 +14,14 @@
   import Text from "$lib/components/common/form-text.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
-  export let p: MeetingInvite;
+  interface Props {
+    p: MeetingInvite;
+  }
 
-  let warning = false;
-  let disabled = false;
+  let { p }: Props = $props();
+
+  let warning = $state(false);
+  let disabled = $state(false);
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -25,7 +29,7 @@
   }
 
   // ---------------------------------------------------------------------------
-  async function onSubmit() {
+  async function onsubmit() {
     try {
       warning = false;
       disabled = true;
@@ -42,7 +46,7 @@
 <!-- -------------------------------------------------------------------------->
 <section id="disable">
   <div class="d-flex mt-2 justify-content-center">
-    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+    <form {onsubmit} style="width:{FORM_WIDTH};">
       <Text
         name="name"
         label="Name"
@@ -90,9 +94,9 @@
       {/if}
 
       <div class="d-flex gap-5 mt-5 justify-content-center">
-        <Cancel bind:disabled on:click={cancel} />
+        <Cancel {disabled} onclick={cancel} />
         <SubmitBlocker />
-        <Submit label="Disable" bind:disabled />
+        <Submit {disabled} label="Disable" />
       </div>
     </form>
   </div>
