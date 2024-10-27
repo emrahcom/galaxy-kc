ada9cf18e85c79e8c69b7045eb14d635f186675a
emrah <emrah.eryilmaz@nordeck.net>
2024-10-27 18:28:58 +0300

    feat(eb-app-ui): sync for Svelte5, contact-friendship/add.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-friendship/add.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/contact-friendship/add.svelte
@@ -9,15 +9,19 @@
   import Text from "$lib/components/common/form-text.svelte";
   import Warning from "$lib/components/common/alert-warning.svelte";
 
-  export let invite: ContactInvite111;
-  export let isExist: boolean;
+  interface Props {
+    invite: ContactInvite111;
+    isExist: boolean;
+  }
 
-  let warning = false;
-  let disabled = false;
-  let p = {
+  let { invite, isExist }: Props = $props();
+
+  let warning = $state(false);
+  let disabled = $state(false);
+  let p = $state({
     code: invite.code,
     name: invite.profile_name,
-  };
+  });
 
   // ---------------------------------------------------------------------------
   function cancel() {
@@ -25,7 +29,7 @@
   }
 
   // ---------------------------------------------------------------------------
-  async function onSubmit() {
+  async function onsubmit() {
     try {
       warning = false;
       disabled = true;
@@ -42,7 +46,7 @@
 <!-- -------------------------------------------------------------------------->
 <section id="add">
   <div class="d-flex mt-2 justify-content-center">
-    <form on:submit|preventDefault={onSubmit} style="width:{FORM_WIDTH};">
+    <form {onsubmit} style="width:{FORM_WIDTH};">
       <Text name="name" label="Contact" bind:value={p.name} required={true} />
       <Text
         name="profile_name"
@@ -70,13 +74,13 @@
         </Warning>
 
         <div class="d-flex gap-5 mt-5 justify-content-center">
-          <Cancel label="Abort" on:click={cancel} />
+          <Cancel label="Abort" onclick={cancel} />
         </div>
       {:else}
         <div class="d-flex gap-5 mt-5 justify-content-center">
-          <Cancel bind:disabled on:click={cancel} />
+          <Cancel {disabled} onclick={cancel} />
           <SubmitBlocker />
-          <Submit label="Add" bind:disabled />
+          <Submit {disabled} label="Add" />
         </div>
       {/if}
     </form>
