224e409296814bff71249e901c2d20d2b9f6c018
emrah <emrah.eryilmaz@nordeck.net>
2024-11-09 18:29:02 +0300

    refactor(eb-app-api): sync config.mailer.ts

--- machines/eb-app-api/home/api/galaxy/config.mailer.ts
+++ machines/eb-app-api/home/api/galaxy/config.mailer.ts
@@ -1,14 +1,16 @@
 // -----------------------------------------------------------------------------
 // Mailer config is not part of the main config.ts to make the system more
-// customizable. The structure of "transportOptions" depends on the mail system.
+// customizable. The structure of "MAILER_TRANSPORT_OPTIONS" depends on the mail
+// system.
 //
-// This config must provide "transportOptions" and "mailFrom" (the sender
-// address).
+// This config must provide "MAILER_TRANSPORT_OPTIONS" and "MAILER_FROM" (the
+// sender address).
+//
 // See https://nodemailer.com/smtp for details.
 // -----------------------------------------------------------------------------
 
 // transporter settings
-export const transportOptions = {
+export const MAILER_TRANSPORT_OPTIONS = {
   host: Deno.env.get("MAILER_HOST") || "",
   port: Number(Deno.env.get("MAILER_PORT") || 465),
   secure: Boolean(Deno.env.get("MAILER_SECURE") || true),
@@ -19,4 +21,4 @@ export const transportOptions = {
 };
 
 // the sender address
-export const mailFrom = Deno.env.get("MAILER_FROM") || "";
+export const MAILER_FROM = Deno.env.get("MAILER_FROM") || "";
