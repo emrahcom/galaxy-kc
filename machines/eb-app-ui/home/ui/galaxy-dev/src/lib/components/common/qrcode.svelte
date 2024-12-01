9503e204eb010246bc16d3fe7533800f8d95aeb2
emrah <emrah.eryilmaz@nordeck.net>
2024-12-01 18:02:47 +0300

    refactor(eb-app-ui): sync common/qrcode.ts

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/common/qrcode.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { toCanvas } from "qrcode";
+
+  interface Props {
+    data: string;
+  }
+
+  let { data }: Props = $props();
+
+  let canvas: HTMLCanvasElement;
+
+  $effect(() => {
+    toCanvas(canvas, data);
+  });
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<canvas bind:this={canvas}></canvas>
