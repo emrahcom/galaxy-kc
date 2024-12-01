c461408d783cbede12934d1473fa598f6d443430
emrah <emrah.eryilmaz@nordeck.net>
2024-12-01 18:31:53 +0300

    refactor(eb-app-ui): sync layout.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/+layout.svelte
@@ -8,7 +8,8 @@
   import Brand from "$lib/components/nav/brand.svelte";
   import NavBarPri from "$lib/components/nav/bar-pri.svelte";
   import NavBarPub from "$lib/components/nav/bar-pub-kc.svelte";
-  import Messages from "$lib/components/message/list.svelte";
+  import Messages from "$lib/components/pri/message/list.svelte";
+  import type { IntercomMessage222 } from "$lib/types";
 
   interface Props {
     children: Snippet;
@@ -16,24 +17,25 @@
 
   let { children }: Props = $props();
 
-  // get active messages while loading
-  let messages = $state(updateMessageList());
-
-  // this event is triggered if a message action happens inside this tab
-  document.addEventListener("internalMessage", () => {
-    messages = updateMessageList();
-  });
-
-  // this event is triggered if a message action happens inside other tabs
-  globalThis.addEventListener("storage", (e) => {
-    if (e.key?.match("^msg-")) {
-      messages = updateMessageList();
-    }
-  });
+  let messages = $state([] as IntercomMessage222[]);
 
   const isAuthenticated =
     globalThis.sessionStorage.getItem("oidc_authenticated");
+
   if (isAuthenticated) {
+    // get active messages while loading if this is an authenticated session
+    messages = updateMessageList();
+
+    // this listener will be triggered if a message event happens in this tab
+    document.addEventListener("internalMessage", () => {
+      messages = updateMessageList();
+    });
+
+    // this listener will be triggered if a message event happens in other tabs
+    globalThis.addEventListener("storage", (e) => {
+      if (e.key?.match("^msg-")) messages = updateMessageList();
+    });
+
     ping();
     intercomHandler();
   }
@@ -63,7 +65,9 @@
   {@render children()}
 </div>
 
-<!-- Messages will be added inside this container -->
-<div aria-live="polite" aria-atomic="true" class="position-relative">
-  <Messages {messages} />
-</div>
+{#if isAuthenticated}
+  <!-- Messages will be added inside this container -->
+  <div aria-live="polite" aria-atomic="true" class="position-relative">
+    <Messages {messages} />
+  </div>
+{/if}
