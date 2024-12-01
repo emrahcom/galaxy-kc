33225fe6bba3ed5f7454cb3dcf118885ad75402e
emrah <emrah.eryilmaz@nordeck.net>
2024-12-01 17:49:33 +0300

    refactor(eb-app-api): sync pub/intercom.ts

---
+++ machines/eb-app-api/home/api/galaxy/lib/pub/intercom.ts
@@ -0,0 +1,49 @@
+import { notFound } from "../http/response.ts";
+import { pub as wrapper } from "../http/wrapper.ts";
+import {
+  delIntercomByCode,
+  getIntercomAttrByCode,
+} from "../database/intercom.ts";
+import { ringPhone } from "../database/intercom-call.ts";
+
+const PRE = "/api/pub/intercom";
+
+// -----------------------------------------------------------------------------
+async function getAttr(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const code = pl.code;
+  const intercomId = pl.id;
+
+  return await getIntercomAttrByCode(code, intercomId);
+}
+
+// -----------------------------------------------------------------------------
+async function del(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const code = pl.code;
+  const intercomId = pl.id;
+
+  return await delIntercomByCode(code, intercomId);
+}
+
+// -----------------------------------------------------------------------------
+async function ring(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const code = pl.code;
+  const intercomId = pl.id;
+
+  return await ringPhone(code, intercomId);
+}
+
+// -----------------------------------------------------------------------------
+export default async function (req: Request, path: string): Promise<Response> {
+  if (path === `${PRE}/get/attr`) {
+    return await wrapper(getAttr, req);
+  } else if (path === `${PRE}/del`) {
+    return await wrapper(del, req);
+  } else if (path === `${PRE}/phone/ring`) {
+    return await wrapper(ring, req);
+  } else {
+    return notFound();
+  }
+}
