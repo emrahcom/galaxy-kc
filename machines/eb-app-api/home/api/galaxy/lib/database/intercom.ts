5043022352b17d97423fb0b1e7b2384961b5052f
emrah <emrah.eryilmaz@nordeck.net>
2024-12-01 17:45:18 +0300

    refactor(eb-app-api): sync database/intercom.ts

--- machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
@@ -1,10 +1,13 @@
 import { fetch } from "./common.ts";
 import type {
+  Attr,
   Id,
   IntercomMessage,
   IntercomMessage222,
   IntercomStatus,
 } from "./types.ts";
+
+const SYSTEM_ACCOUNT = "00000000-0000-0000-0000-000000000000";
 
 // -----------------------------------------------------------------------------
 export async function getIntercomForOwner(
@@ -28,13 +31,14 @@ export async function getIntercomForOwner(
 
 // -----------------------------------------------------------------------------
 export async function getIntercom(identityId: string, intercomId: string) {
+  // There will be no contact if this is a public phone call.
   const sql = {
     text: `
-      SELECT ic.id, co.id as contact_id, co.name as contact_name, status,
-        message_type, intercom_attr, expired_at
+      SELECT ic.id, co.name as contact_name, status, message_type,
+        intercom_attr, expired_at
       FROM intercom ic
-        JOIN contact co ON co.identity_id = $1
-                            AND co.remote_id = ic.identity_id
+        LEFT JOIN contact co ON co.identity_id = $1
+                                AND co.remote_id = ic.identity_id
       WHERE ic.id = $2
         AND ic.remote_id = $1`,
     args: [
@@ -47,18 +51,44 @@ export async function getIntercom(identityId: string, intercomId: string) {
 }
 
 // -----------------------------------------------------------------------------
+// consumer is public
+// -----------------------------------------------------------------------------
+export async function getIntercomAttrByCode(code: string, intercomId: string) {
+  const sql = {
+    text: `
+      SELECT intercom_attr->>'public_url' as public_url
+      FROM intercom
+      WHERE id = $2
+        AND identity_id = $3
+        AND status = 'accepted'
+        AND remote_id = (SELECT identity_id
+                         FROM phone
+                         WHERE code = $1
+                        )`,
+    args: [
+      code,
+      intercomId,
+      SYSTEM_ACCOUNT,
+    ],
+  };
+
+  return await fetch(sql) as Attr[];
+}
+
+// -----------------------------------------------------------------------------
 export async function listIntercom(
   identityId: string,
   limit: number,
   offset: number,
 ) {
+  // There will be no contact if the record is for a public phone call.
   const sql = {
     text: `
-      SELECT ic.id, co.id as contact_id, co.name as contact_name, status,
-        message_type, intercom_attr, expired_at
+      SELECT ic.id, co.name as contact_name, status, message_type,
+        intercom_attr, expired_at
       FROM intercom ic
-        JOIN contact co ON co.identity_id = $1
-                           AND co.remote_id = ic.identity_id
+        LEFT JOIN contact co ON co.identity_id = $1
+                                AND co.remote_id = ic.identity_id
       WHERE ic.remote_id = $1
         AND expired_at > now()
         AND status = 'none'
@@ -84,6 +114,28 @@ export async function delIntercom(identityId: string, intercomId: string) {
     args: [
       identityId,
       intercomId,
+    ],
+  };
+
+  return await fetch(sql) as Id[];
+}
+
+// -----------------------------------------------------------------------------
+export async function delIntercomByCode(code: string, intercomId: string) {
+  const sql = {
+    text: `
+      DELETE FROM intercom
+      WHERE id = $2
+        AND identity_id = $3
+        AND remote_id = (SELECT identity_id
+                         FROM phone
+                         WHERE code = $1
+                        )
+      RETURNING id, now() as at`,
+    args: [
+      code,
+      intercomId,
+      SYSTEM_ACCOUNT,
     ],
   };
 
