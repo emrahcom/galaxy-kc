b7ed08a0d911997861c84f79bd7c387afd1b2c6a
emrah <emrah.eryilmaz@nordeck.net>
2025-01-10 18:43:59 +0300

    refactor(eb-app-ui): sync identity/key/disable

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/identity/key/disable/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Disable from "$lib/components/pri/identity-key/disable.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/identity/key/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Disable this private key" />
+
+{#await pr then key}
+  <Disable p={key} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
