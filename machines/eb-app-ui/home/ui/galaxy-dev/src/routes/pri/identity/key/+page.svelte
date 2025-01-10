1fbfe80c19bfbea7c319ff0865bcc62cc6688663
emrah <emrah.eryilmaz@nordeck.net>
2025-01-10 18:44:32 +0300

    refactor(eb-app-ui): sync identity/key/page.svelte

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/identity/key/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { list } from "$lib/api";
+  import List from "$lib/components/pri/identity-key/list.svelte";
+  import Subheader from "$lib/components/common/subheader.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = list("/api/pri/identity/key/list", 100);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader
+  subheader="My private keys"
+  hrefAdd="/pri/identity/key/add"
+  hrefAddTitle="Add a new private key"
+/>
+
+{#await pr then keys}
+  <List {keys} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
