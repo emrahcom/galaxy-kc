fd778913ce7f8f4150148c9fffd5aa84973b9526
emrah <emrah.eryilmaz@nordeck.net>
2025-01-10 18:37:49 +0300

    refactor(eb-app-ui): sync identity-key/list.svelte

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/identity-key/list.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { IdentityKey333 } from "$lib/types";
+  import Add from "$lib/components/common/link-add.svelte";
+  import Item from "$lib/components/pri/identity-key/list-item.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  interface Props {
+    keys: IdentityKey333[];
+  }
+
+  let { keys }: Props = $props();
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="list">
+  <div class="row mx-auto mt-2 g-3">
+    {#each keys as p}
+      <Item {p} />
+    {:else}
+      <Warning>
+        There is no key in the list. Click <Add href="/pri/identity/key/add" />
+        to add a new private key.
+      </Warning>
+    {/each}
+  </div>
+</section>
