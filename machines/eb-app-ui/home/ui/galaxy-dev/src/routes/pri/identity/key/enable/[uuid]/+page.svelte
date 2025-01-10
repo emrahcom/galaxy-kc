8ee119a67736651cd6d397f2449f77d78d6c3caf
emrah <emrah.eryilmaz@nordeck.net>
2025-01-10 18:44:10 +0300

    refactor(eb-app-ui): sync identity/key/enable

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/routes/pri/identity/key/enable/[uuid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { getById } from "$lib/api";
+  import Enable from "$lib/components/pri/identity-key/enable.svelte";
+  import Subheader from "$lib/components/common/subheader-center.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  const pr = getById("/api/pri/identity/key/get", $page.params.uuid);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<Subheader subheader="Enable this private key" />
+
+{#await pr then key}
+  <Enable p={key} />
+{:catch}
+  <Warning>Something went wrong</Warning>
+{/await}
