f50fd712891ea5d9e52a22b47abd1b5872483aed
emrah <emrah.eryilmaz@nordeck.net>
2025-01-10 18:37:18 +0300

    refactor(eb-app-ui): sync identity-key/del.svelte

---
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/identity-key/del.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+  import { FORM_WIDTH } from "$lib/config";
+  import { actionById } from "$lib/api";
+  import type { IdentityKey } from "$lib/types";
+  import Cancel from "$lib/components/common/button-cancel.svelte";
+  import Submit from "$lib/components/common/button-submit.svelte";
+  import SubmitBlocker from "$lib/components/common/button-submit-blocker.svelte";
+  import Text from "$lib/components/common/form-text.svelte";
+  import Warning from "$lib/components/common/alert-warning.svelte";
+
+  interface Props {
+    p: IdentityKey;
+  }
+
+  let { p }: Props = $props();
+
+  let warning = $state(false);
+  let disabled = $state(false);
+
+  if (!p.domain_enabled) {
+    p.domain_name = `${p.domain_name} - DISABLED`;
+  }
+
+  // ---------------------------------------------------------------------------
+  function cancel() {
+    globalThis.location.href = "/pri/identity/key";
+  }
+
+  // ---------------------------------------------------------------------------
+  async function onsubmit() {
+    try {
+      warning = false;
+      disabled = true;
+
+      await actionById("/api/pri/identity/key/del", p.id);
+      globalThis.location.replace("/pri/identity/key");
+    } catch {
+      warning = true;
+      disabled = false;
+    }
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<section id="del">
+  <div class="d-flex mt-2 justify-content-center">
+    <form {onsubmit} style="width:{FORM_WIDTH};">
+      <Text
+        name="name"
+        label="Name"
+        value={p.name}
+        disabled={true}
+        readonly={true}
+      />
+      <Text
+        name="domain_name"
+        label="Jitsi Domain Name"
+        value={p.domain_name}
+        disabled={true}
+        readonly={true}
+      />
+      <Text
+        name="domain_url"
+        label="Jitsi Domain URL"
+        value={p.domain_url}
+        disabled={true}
+        readonly={true}
+      />
+
+      {#if warning}
+        <Warning>The delete request is not accepted.</Warning>
+      {/if}
+
+      <div class="d-flex gap-5 mt-5 justify-content-center">
+        <Cancel {disabled} onclick={cancel} />
+        <SubmitBlocker />
+        <Submit {disabled} label="Delete" />
+      </div>
+    </form>
+  </div>
+</section>
