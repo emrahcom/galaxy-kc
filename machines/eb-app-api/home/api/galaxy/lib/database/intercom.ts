f68bc3f2a90dd872fdd0047c48fabe8efd67bc18
emrah <emrah.eryilmaz@nordeck.net>
2025-01-10 18:18:33 +0300

    refactor(eb-app-api): sync database/intercom.ts

--- machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
@@ -10,6 +10,8 @@ import type {
 const SYSTEM_ACCOUNT = "00000000-0000-0000-0000-000000000000";
 
 // -----------------------------------------------------------------------------
+// Consumer is the caller who creates the record.
+// -----------------------------------------------------------------------------
 export async function getIntercomForOwner(
   identityId: string,
   intercomId: string,
@@ -30,14 +32,17 @@ export async function getIntercomForOwner(
 }
 
 // -----------------------------------------------------------------------------
+// Consumer is the callee.
+// -----------------------------------------------------------------------------
 export async function getIntercom(identityId: string, intercomId: string) {
-  // There will be no contact if this is a public phone call.
+  // There will be no contact if this call is from a public phone.
+  // ic.identity_id is the system user in this case.
   const sql = {
     text: `
       SELECT ic.id, co.name as contact_name, status, message_type,
         intercom_attr, expired_at
       FROM intercom ic
-        LEFT JOIN contact co ON co.identity_id = $1
+        LEFT JOIN contact co ON co.identity_id = ic.remote_id
                                 AND co.remote_id = ic.identity_id
       WHERE ic.id = $2
         AND ic.remote_id = $1`,
@@ -51,7 +56,7 @@ export async function getIntercom(identityId: string, intercomId: string) {
 }
 
 // -----------------------------------------------------------------------------
-// consumer is public
+// Consumer is a public user by using a public phone code.
 // -----------------------------------------------------------------------------
 export async function getIntercomAttrByCode(code: string, intercomId: string) {
   const sql = {
@@ -76,18 +81,21 @@ export async function getIntercomAttrByCode(code: string, intercomId: string) {
 }
 
 // -----------------------------------------------------------------------------
+// Consumer is the callee.
+// -----------------------------------------------------------------------------
 export async function listIntercom(
   identityId: string,
   limit: number,
   offset: number,
 ) {
-  // There will be no contact if the record is for a public phone call.
+  // There will be no contact if the call is from a public phone.
+  // ic.identity_id is the system user in this case.
   const sql = {
     text: `
       SELECT ic.id, co.name as contact_name, status, message_type,
         intercom_attr, expired_at
       FROM intercom ic
-        LEFT JOIN contact co ON co.identity_id = $1
+        LEFT JOIN contact co ON co.identity_id = ic.remote_id
                                 AND co.remote_id = ic.identity_id
       WHERE ic.remote_id = $1
         AND expired_at > now()
@@ -103,6 +111,43 @@ export async function listIntercom(
   return await fetch(sql) as IntercomMessage222[];
 }
 
+// -----------------------------------------------------------------------------
+// Consumer is the callee with an identity key.
+// -----------------------------------------------------------------------------
+export async function listIntercomByKey(
+  keyValue: string,
+  limit: number,
+  offset: number,
+) {
+  // There will be no contact if the call is from a public phone.
+  // ic.identity_id is the system user in this case.
+  const sql = {
+    text: `
+      SELECT ic.id, co.name as contact_name, status, message_type,
+        intercom_attr, expired_at
+      FROM intercom ic
+        LEFT JOIN contact co ON co.identity_id = ic.remote_id
+                                AND co.remote_id = ic.identity_id
+      WHERE ic.remote_id = (SELECT identity_id
+                            FROM identity_key
+                            WHERE value = $1
+                              AND enabled
+                           )
+        AND expired_at > now()
+        AND status = 'none'
+      LIMIT $2 OFFSET $3`,
+    args: [
+      keyValue,
+      limit,
+      offset,
+    ],
+  };
+
+  return await fetch(sql) as IntercomMessage222[];
+}
+
+// -----------------------------------------------------------------------------
+// Consumer is the caller who creates the record.
 // -----------------------------------------------------------------------------
 export async function delIntercom(identityId: string, intercomId: string) {
   const sql = {
@@ -120,6 +165,31 @@ export async function delIntercom(identityId: string, intercomId: string) {
   return await fetch(sql) as Id[];
 }
 
+// -----------------------------------------------------------------------------
+// Consumer is the caller with an identity key.
+// -----------------------------------------------------------------------------
+export async function delIntercomByKey(keyValue: string, intercomId: string) {
+  const sql = {
+    text: `
+      DELETE FROM intercom
+      WHERE id = $2
+        AND identity_id = (SELECT identity_id
+                           FROM identity_key
+                           WHERE value = $1
+                             AND enabled
+                          )
+      RETURNING id, now() as at`,
+    args: [
+      keyValue,
+      intercomId,
+    ],
+  };
+
+  return await fetch(sql) as Id[];
+}
+
+// -----------------------------------------------------------------------------
+// Consumer is a public user with a public phone code.
 // -----------------------------------------------------------------------------
 export async function delIntercomByCode(code: string, intercomId: string) {
   const sql = {
@@ -143,6 +213,8 @@ export async function delIntercomByCode(code: string, intercomId: string) {
 }
 
 // -----------------------------------------------------------------------------
+// Consumer is the callee.
+// -----------------------------------------------------------------------------
 export async function setStatusIntercom(
   identityId: string,
   intercomId: string,
@@ -157,6 +229,35 @@ export async function setStatusIntercom(
       RETURNING id, now() as at`,
     args: [
       identityId,
+      intercomId,
+      status,
+    ],
+  };
+
+  return await fetch(sql) as Id[];
+}
+
+// -----------------------------------------------------------------------------
+// Consumer is the callee with an identity key.
+// -----------------------------------------------------------------------------
+export async function setStatusIntercomByKey(
+  keyValue: string,
+  intercomId: string,
+  status: IntercomStatus,
+) {
+  const sql = {
+    text: `
+      UPDATE intercom
+      SET status = $3
+      WHERE id = $2
+        AND remote_id = (SELECT identity_id
+                         FROM identity_key
+                         WHERE value = $1
+                           AND enabled
+                        )
+      RETURNING id, now() as at`,
+    args: [
+      keyValue,
       intercomId,
       status,
     ],
