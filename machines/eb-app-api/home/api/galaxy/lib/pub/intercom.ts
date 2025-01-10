5d0c17fc9643fe2c30f4202e8c48a80f0ea176a6
emrah <emrah.eryilmaz@nordeck.net>
2025-01-10 18:27:09 +0300

    refactor(eb-app-api): sync pub/intercom.ts

--- machines/eb-app-api/home/api/galaxy/lib/pub/intercom.ts
+++ machines/eb-app-api/home/api/galaxy/lib/pub/intercom.ts
@@ -1,15 +1,22 @@
+import { getLimit, getOffset } from "../database/common.ts";
 import { notFound } from "../http/response.ts";
 import { pub as wrapper } from "../http/wrapper.ts";
+import { mailMissedCall } from "../common/mail.ts";
+import { getIdentityByKey } from "../database/identity.ts";
 import {
   delIntercomByCode,
+  delIntercomByKey,
   getIntercomAttrByCode,
+  getIntercomForOwner,
+  listIntercomByKey,
+  setStatusIntercomByKey,
 } from "../database/intercom.ts";
-import { ringPhone } from "../database/intercom-call.ts";
+import { ringCallByKey, ringPhoneByCode } from "../database/intercom-call.ts";
 
 const PRE = "/api/pub/intercom";
 
 // -----------------------------------------------------------------------------
-async function getAttr(req: Request): Promise<unknown> {
+async function getAttrByCode(req: Request): Promise<unknown> {
   const pl = await req.json();
   const code = pl.code;
   const intercomId = pl.id;
@@ -18,7 +25,35 @@ async function getAttr(req: Request): Promise<unknown> {
 }
 
 // -----------------------------------------------------------------------------
-async function del(req: Request): Promise<unknown> {
+async function listByKey(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const keyValue = pl.key_value;
+  const limit = getLimit(pl.limit);
+  const offset = getOffset(pl.offset);
+
+  return await listIntercomByKey(keyValue, limit, offset);
+}
+
+// -----------------------------------------------------------------------------
+async function setAcceptedByKey(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const keyValue = pl.key_value;
+  const intercomId = pl.id;
+
+  return await setStatusIntercomByKey(keyValue, intercomId, "accepted");
+}
+
+// -----------------------------------------------------------------------------
+async function setRejectedByKey(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const keyValue = pl.key_value;
+  const intercomId = pl.id;
+
+  return await setStatusIntercomByKey(keyValue, intercomId, "rejected");
+}
+
+// -----------------------------------------------------------------------------
+async function delByCode(req: Request): Promise<unknown> {
   const pl = await req.json();
   const code = pl.code;
   const intercomId = pl.id;
@@ -27,22 +62,73 @@ async function del(req: Request): Promise<unknown> {
 }
 
 // -----------------------------------------------------------------------------
-async function ring(req: Request): Promise<unknown> {
+async function delByKey(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const keyValue = pl.key_value;
+  const intercomId = pl.id;
+
+  return await delIntercomByKey(keyValue, intercomId);
+}
+
+// -----------------------------------------------------------------------------
+async function delWithNotificationByKey(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const keyValue = pl.key_value;
+  const intercomId = pl.id;
+
+  const identities = await getIdentityByKey(keyValue);
+  const identity = identities[0];
+  const identityId = identity.id;
+
+  const intercomMessages = await getIntercomForOwner(identityId, intercomId);
+  const intercomMessage = intercomMessages[0];
+
+  if (intercomMessage?.remote_id) {
+    // dont wait for the async function
+    mailMissedCall(identityId, intercomMessage.remote_id);
+  }
+
+  return await delIntercomByKey(keyValue, intercomId);
+}
+
+// -----------------------------------------------------------------------------
+async function ringByKey(req: Request): Promise<unknown> {
+  const pl = await req.json();
+  const keyValue = pl.key_value;
+  const intercomId = pl.id;
+
+  return await ringCallByKey(keyValue, intercomId);
+}
+
+// -----------------------------------------------------------------------------
+async function ringByCode(req: Request): Promise<unknown> {
   const pl = await req.json();
   const code = pl.code;
   const intercomId = pl.id;
 
-  return await ringPhone(code, intercomId);
+  return await ringPhoneByCode(code, intercomId);
 }
 
 // -----------------------------------------------------------------------------
 export default async function (req: Request, path: string): Promise<Response> {
-  if (path === `${PRE}/get/attr`) {
-    return await wrapper(getAttr, req);
-  } else if (path === `${PRE}/del`) {
-    return await wrapper(del, req);
-  } else if (path === `${PRE}/phone/ring`) {
-    return await wrapper(ring, req);
+  if (path === `${PRE}/get/attr/bycode`) {
+    return await wrapper(getAttrByCode, req);
+  } else if (path === `${PRE}/list/bykey`) {
+    return await wrapper(listByKey, req);
+  } else if (path === `${PRE}/del/bycode`) {
+    return await wrapper(delByCode, req);
+  } else if (path === `${PRE}/del/bykey`) {
+    return await wrapper(delByKey, req);
+  } else if (path === `${PRE}/del-with-notification/bykey`) {
+    return await wrapper(delWithNotificationByKey, req);
+  } else if (path === `${PRE}/set/accepted/bykey`) {
+    return await wrapper(setAcceptedByKey, req);
+  } else if (path === `${PRE}/set/rejected/bykey`) {
+    return await wrapper(setRejectedByKey, req);
+  } else if (path === `${PRE}/call/ring/bykey`) {
+    return await wrapper(ringByKey, req);
+  } else if (path === `${PRE}/phone/ring/bycode`) {
+    return await wrapper(ringByCode, req);
   } else {
     return notFound();
   }
