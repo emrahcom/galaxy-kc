42ebf3ae42169be7b5c76df9fd243e2ce112e6a4
emrah <emrah.eryilmaz@conceptboard.com>
2025-08-31 18:24:10 +0300

    refactor(eb-app-ui): sync pri/intercom.ts

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/intercom.ts
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/pri/intercom.ts
@@ -1,10 +1,10 @@
-import { getById, list } from "$lib/api";
+import { getById, listByValue } from "$lib/api";
 import { isOver } from "$lib/common";
 import type { IntercomMessage222 } from "$lib/types";
 
 // -----------------------------------------------------------------------------
 export function updateMessageList() {
-  const list: IntercomMessage222[] = [];
+  const messages: IntercomMessage222[] = [];
 
   for (const key in globalThis.localStorage) {
     try {
@@ -14,41 +14,55 @@ export function updateMessageList() {
       if (!value) throw "empty message";
 
       const parsedValue = JSON.parse(value) as IntercomMessage222;
-      list.push(parsedValue);
+      messages.push(parsedValue);
     } catch {
       globalThis.localStorage.removeItem(key);
     }
   }
 
-  return list;
+  // Call and phone messages are always before the text messages.
+  // The text messages should be ordered according to their creation times.
+  const sortedMessages = [...messages].sort((a, b) => {
+    let dateA = Number(a.microsec_created_at) || 0;
+    let dateB = Number(b.microsec_created_at) || 0;
+
+    // Use the time of 8 hours earlier for non-text messages to show them before
+    // the text messages.
+    if (a.message_type !== "text") dateA = dateA - 8 * 3600 * 1000000;
+    if (b.message_type !== "text") dateB = dateB - 8 * 3600 * 1000000;
+
+    if (dateA > dateB) return -1;
+    else if (dateA < dateB) return 1;
+    else return 0;
+  });
+
+  // Show only the first 5 messages.
+  // Phone and direct calls have always higher priority.
+  return sortedMessages.slice(-5);
 }
 
 // -----------------------------------------------------------------------------
-export async function watchCall(msgId: string) {
+// The last message time will be used as the starting poing in the next request.
+// -----------------------------------------------------------------------------
+function setLastMessageTime(msg: IntercomMessage222) {
   try {
-    // this will fail if the call message is already deleted
-    const msg = await getById("/api/pri/intercom/get", msgId);
-    if (msg.status !== "none") throw "invalid status";
+    const last = globalThis.localStorage.getItem("intercom_last_msg_at");
 
-    const expiredAt = new Date(msg.expired_at);
-    if (isOver(expiredAt)) throw "expired call";
-
-    setTimeout(() => {
-      watchCall(msgId);
-    }, 2000);
+    if (isNaN(Number(last)) || msg.microsec_created_at > Number(last)) {
+      globalThis.localStorage.setItem(
+        "intercom_last_msg_at",
+        String(msg.microsec_created_at),
+      );
+    }
   } catch {
-    delCallMessage(msgId);
+    // do nothing
   }
 }
 
 // -----------------------------------------------------------------------------
-// watchPhone is an alias for watchCall.
-// Their logics are completely the same.
+// Add the message object to the local storage.
 // -----------------------------------------------------------------------------
-export const watchPhone = watchCall;
-
-// -----------------------------------------------------------------------------
-function addCallMessage(msg: IntercomMessage222) {
+function addMessage(msg: IntercomMessage222) {
   try {
     const isExist = globalThis.localStorage.getItem(`msg-${msg.id}`);
     if (isExist) return;
@@ -61,18 +75,59 @@ function addCallMessage(msg: IntercomMessage222) {
 }
 
 // -----------------------------------------------------------------------------
-// addPhoneMessage is an alias for addCallMessage.
-// Their logics are completely the same.
+// Remove the message object from the local storage.
 // -----------------------------------------------------------------------------
-const addPhoneMessage = addCallMessage;
-
-// -----------------------------------------------------------------------------
-function delCallMessage(msgId: string) {
+export function delMessage(msgId: string) {
   try {
     globalThis.localStorage.removeItem(`msg-${msgId}`);
     document.dispatchEvent(new CustomEvent("internalMessage"));
   } catch {
     // do nothing
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Watching will happen inside the popup when the message becomes visible.
+// No watching for unvisible messages.
+// -----------------------------------------------------------------------------
+export async function watchMessage(msgId: string, interval = 2000) {
+  try {
+    // This will fail if the message is already deleted.
+    const msg = await getById("/api/pri/intercom/get", msgId);
+    if (msg.status !== "none") throw "invalid status";
+
+    const expiredAt = new Date(msg.expired_at);
+    if (isOver(expiredAt)) throw "expired message";
+
+    setTimeout(() => {
+      watchMessage(msgId, interval);
+    }, interval);
+  } catch {
+    delMessage(msgId);
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Watching will happen inside the popup when the message becomes visible.
+// No watching for unvisible messages. There is a ramp up in text message
+// watching.
+// -----------------------------------------------------------------------------
+export async function watchTextMessage(msgId: string, interval = 2000) {
+  try {
+    // This will fail if the message is already deleted.
+    const msg = await getById("/api/pri/intercom/get", msgId);
+    if (msg.status !== "none") throw "invalid status";
+
+    const expiredAt = new Date(msg.expired_at);
+    if (isOver(expiredAt)) throw "expired message";
+
+    const nextInterval = 2 * interval > 600000 ? 600000 : 2 * interval;
+
+    setTimeout(() => {
+      watchTextMessage(msgId, nextInterval);
+    }, nextInterval);
+  } catch {
+    delMessage(msgId);
   }
 }
 
@@ -92,16 +147,20 @@ export async function intercomHandler() {
     if (now - Number(checkedAt) > 3000) {
       globalThis.localStorage.setItem("intercom_checked_at", String(now));
 
-      const messages: IntercomMessage222[] = await list(
+      const messages: IntercomMessage222[] = await listByValue(
         "/api/pri/intercom/list",
-        10,
+        globalThis.localStorage.getItem("intercom_last_msg_at") || "0",
       );
 
       for (const msg of messages) {
-        if (msg.message_type === "call") {
-          addCallMessage(msg);
-        } else if (msg.message_type === "phone") {
-          addPhoneMessage(msg);
+        setLastMessageTime(msg);
+
+        if (
+          msg.message_type === "text" ||
+          msg.message_type === "call" ||
+          msg.message_type === "phone"
+        ) {
+          addMessage(msg);
         }
       }
     }
