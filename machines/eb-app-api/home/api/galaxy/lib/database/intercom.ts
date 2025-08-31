f66f4a26fe29af83aa1320e2452228ac7f5afb85
emrah <emrah.eryilmaz@conceptboard.com>
2025-08-31 18:12:58 +0300

    refactor(eb-app-api): sync database/intercom.ts

--- machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
+++ machines/eb-app-api/home/api/galaxy/lib/database/intercom.ts
@@ -33,14 +33,31 @@ export async function getIntercomForOwner(
 
 // -----------------------------------------------------------------------------
 // Consumer is the callee.
+//
+// Epoch time in query output and on the client-side is microsecond (as integer)
+// but second (as float) in Postgres...
 // -----------------------------------------------------------------------------
 export async function getIntercom(identityId: string, intercomId: string) {
   // There will be no contact if this call is from a public phone.
   // ic.identity_id is the system user in this case.
   const sql = {
     text: `
-      SELECT ic.id, co.name as contact_name, status, message_type,
-        intercom_attr, expired_at
+      SELECT ic.id, co.name as contact_name, ic.status, ic.message_type,
+        CASE
+          WHEN ic.message_type = 'text' THEN
+            jsonb_build_object(
+              'message', convert_from(
+                           decode(ic.intercom_attr->>'message', 'base64'),
+                           'UTF8'
+                         )
+            )
+          ELSE ic.intercom_attr
+        END AS intercom_attr,
+        ic.created_at,
+        TRUNC(
+          EXTRACT(EPOCH FROM ic.created_at) * 1000000
+        ) AS microsec_created_at,
+        ic.expired_at
       FROM intercom ic
         LEFT JOIN contact co ON co.identity_id = ic.remote_id
                                 AND co.remote_id = ic.identity_id
@@ -48,6 +65,54 @@ export async function getIntercom(identityId: string, intercomId: string) {
         AND ic.remote_id = $1`,
     args: [
       identityId,
+      intercomId,
+    ],
+  };
+
+  return await fetch(sql) as IntercomMessage222[];
+}
+
+// -----------------------------------------------------------------------------
+// Consumer is the callee with an identity key.
+//
+// Epoch time in query output and on the client-side is microsecond (as integer)
+// but second (as float) in Postgres...
+//
+// message in jsonb is base64 encoded. Not because of the security reason, to
+// get clear output in database backups.
+// -----------------------------------------------------------------------------
+export async function getIntercomByKey(keyValue: string, intercomId: string) {
+  // There will be no contact if this call is from a public phone.
+  // ic.identity_id is the system user in this case.
+  const sql = {
+    text: `
+      SELECT ic.id, co.name as contact_name, ic.status, ic.message_type,
+        CASE
+          WHEN ic.message_type = 'text' THEN
+            jsonb_build_object(
+              'message', convert_from(
+                           decode(ic.intercom_attr->>'message', 'base64'),
+                           'UTF8'
+                         )
+            )
+          ELSE ic.intercom_attr
+        END AS intercom_attr,
+        ic.created_at,
+        TRUNC(
+          EXTRACT(EPOCH FROM ic.created_at) * 1000000
+        ) AS microsec_created_at,
+        ic.expired_at
+      FROM intercom ic
+        LEFT JOIN contact co ON co.identity_id = ic.remote_id
+                                AND co.remote_id = ic.identity_id
+      WHERE ic.id = $2
+        AND ic.remote_id = (SELECT identity_id
+                            FROM identity_key
+                            WHERE value = $1
+                              AND enabled
+                           )`,
+    args: [
+      keyValue,
       intercomId,
     ],
   };
@@ -82,9 +147,16 @@ export async function getIntercomAttrByCode(code: string, intercomId: string) {
 
 // -----------------------------------------------------------------------------
 // Consumer is the callee.
+//
+// Epoch time in query output and on the client-side is microsecond (as integer)
+// but second (as float) in Postgres...
+//
+// message in jsonb is base64 encoded. Not because of the security reason, to
+// get clear output in database backups.
 // -----------------------------------------------------------------------------
 export async function listIntercom(
   identityId: string,
+  microsec: number,
   limit: number,
   offset: number,
 ) {
@@ -92,17 +164,41 @@ export async function listIntercom(
   // ic.identity_id is the system user in this case.
   const sql = {
     text: `
-      SELECT ic.id, co.name as contact_name, status, message_type,
-        intercom_attr, expired_at
+      SELECT ic.id, co.name as contact_name, ic.status, ic.message_type,
+        CASE
+          WHEN ic.message_type = 'text' THEN
+            jsonb_build_object(
+              'message', convert_from(
+                           decode(ic.intercom_attr->>'message', 'base64'),
+                           'UTF8'
+                         )
+            )
+          ELSE ic.intercom_attr
+        END AS intercom_attr,
+        ic.created_at,
+        TRUNC(
+          EXTRACT(EPOCH FROM ic.created_at) * 1000000
+        ) AS microsec_created_at,
+        ic.expired_at
       FROM intercom ic
         LEFT JOIN contact co ON co.identity_id = ic.remote_id
                                 AND co.remote_id = ic.identity_id
       WHERE ic.remote_id = $1
-        AND expired_at > now()
-        AND status = 'none'
-      LIMIT $2 OFFSET $3`,
+        AND ic.expired_at > now()
+        AND ic.status = 'none'
+        AND ic.created_at > to_timestamp($2)
+      ORDER BY
+        CASE ic.message_type
+          WHEN 'call' THEN 1
+          WHEN 'phone' THEN 1
+          WHEN 'text' THEN 2
+          ELSE 3
+        END,
+        ic.created_at
+      LIMIT $3 OFFSET $4`,
     args: [
       identityId,
+      microsec / 1000000,
       limit,
       offset,
     ],
@@ -113,9 +209,18 @@ export async function listIntercom(
 
 // -----------------------------------------------------------------------------
 // Consumer is the callee with an identity key.
+//
+// Epoch time in query output and on the client-side is microsecond (as integer)
+// but second (as float) in Postgres...
+//
+// microsec is expected as 0 in the current Chrome extension implementation.
+//
+// message in jsonb is base64 encoded. Not because of the security reason, to
+// get clear output in database backups.
 // -----------------------------------------------------------------------------
 export async function listIntercomByKey(
   keyValue: string,
+  microsec: number,
   limit: number,
   offset: number,
 ) {
@@ -123,8 +228,22 @@ export async function listIntercomByKey(
   // ic.identity_id is the system user in this case.
   const sql = {
     text: `
-      SELECT ic.id, co.name as contact_name, status, message_type,
-        intercom_attr, expired_at
+      SELECT ic.id, co.name as contact_name, ic.status, ic.message_type,
+        CASE
+          WHEN ic.message_type = 'text' THEN
+            jsonb_build_object(
+              'message', convert_from(
+                           decode(ic.intercom_attr->>'message', 'base64'),
+                           'UTF8'
+                         )
+            )
+          ELSE ic.intercom_attr
+        END AS intercom_attr,
+        ic.created_at,
+        TRUNC(
+          EXTRACT(EPOCH FROM ic.created_at) * 1000000
+        ) AS microsec_created_at,
+        ic.expired_at
       FROM intercom ic
         LEFT JOIN contact co ON co.identity_id = ic.remote_id
                                 AND co.remote_id = ic.identity_id
@@ -133,11 +252,21 @@ export async function listIntercomByKey(
                             WHERE value = $1
                               AND enabled
                            )
-        AND expired_at > now()
-        AND status = 'none'
-      LIMIT $2 OFFSET $3`,
+        AND ic.expired_at > now()
+        AND ic.status = 'none'
+        AND ic.created_at > to_timestamp($2)
+      ORDER BY
+        CASE ic.message_type
+          WHEN 'call' THEN 1
+          WHEN 'phone' THEN 1
+          WHEN 'text' THEN 2
+          ELSE 3
+        END,
+        ic.created_at
+      LIMIT $3 OFFSET $4`,
     args: [
       keyValue,
+      microsec / 1000000,
       limit,
       offset,
     ],
@@ -219,6 +348,7 @@ export async function setStatusIntercom(
   identityId: string,
   intercomId: string,
   status: IntercomStatus,
+  ifNone = false,
 ) {
   const sql = {
     text: `
@@ -226,11 +356,13 @@ export async function setStatusIntercom(
       SET status = $3
       WHERE id = $2
         AND remote_id = $1
+        AND (status = 'none' OR $4 is false)
       RETURNING id, now() as at`,
     args: [
       identityId,
       intercomId,
       status,
+      ifNone,
     ],
   };
 
@@ -244,6 +376,7 @@ export async function setStatusIntercomByKey(
   keyValue: string,
   intercomId: string,
   status: IntercomStatus,
+  ifNone = false,
 ) {
   const sql = {
     text: `
@@ -255,11 +388,13 @@ export async function setStatusIntercomByKey(
                          WHERE value = $1
                            AND enabled
                         )
+        AND (status = 'none' OR $4 is false)
       RETURNING id, now() as at`,
     args: [
       keyValue,
       intercomId,
       status,
+      ifNone,
     ],
   };
 
