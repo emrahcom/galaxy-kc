b71d04ff94673bb66a474fde175cd5376efb424e
emrah <emrah.eryilmaz@conceptboard.com>
2025-08-31 18:22:19 +0300

    refactor(eb-app-ui): sync components/pri/message/list.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/message/list.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/message/list.svelte
@@ -2,6 +2,7 @@
   import type { IntercomMessage222 } from "$lib/types";
   import Call from "$lib/components/pri/message/call.svelte";
   import Phone from "$lib/components/pri/message/phone.svelte";
+  import Text from "$lib/components/pri/message/text.svelte";
 
   interface Props {
     messages: IntercomMessage222[];
@@ -11,12 +12,14 @@
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<div id="messages" class="toast-container bottom-0 end-0 p-3">
+<div id="messages" class="toast-container bottom-0 end-0 p-3 bg-white">
   {#each messages as msg (msg.id)}
     {#if msg.message_type === "call"}
       <Call {msg} />
     {:else if msg.message_type === "phone"}
       <Phone {msg} />
+    {:else if msg.message_type === "text"}
+      <Text {msg} />
     {/if}
   {/each}
 </div>
