2ea8a885d91e474e691712bbbb3ab83d059b231a
emrah <emrah.eryilmaz@conceptboard.com>
2025-09-27 17:34:16 +0300

    refactor(eb-app-api): imports in token-kc.ts

--- machines/eb-app-api/home/api/galaxy/lib/common/token-kc.ts
+++ machines/eb-app-api/home/api/galaxy/lib/common/token-kc.ts
@@ -1,6 +1,6 @@
-import { create, getNumericDate } from "jsr:@emrahcom/jwt";
-import type { Payload } from "jsr:@emrahcom/jwt";
-import type { Algorithm } from "jsr:@emrahcom/jwt/algorithm";
+import { create, getNumericDate } from "@emrahcom/jwt";
+import type { Payload } from "@emrahcom/jwt";
+import type { Algorithm } from "@emrahcom/jwt/algorithm";
 import { generateCryptoKeyHS } from "./token.ts";
 import { API_SECRET, API_TIMEOUT } from "../../config.ts";
 
