989a4ec36d8ccbd6646fef1f50fb0c985ebda412
emrah <emrah.eryilmaz@conceptboard.com>
2026-02-14 18:18:36 +0300

    refactor(eb-app-ui): sync pri/domain-invite/add.svelte

--- machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-invite/add.svelte
+++ machines/eb-app-ui/home/ui/galaxy-dev/src/lib/components/pri/domain-invite/add.svelte
@@ -12,7 +12,7 @@
     domain: Domain;
   }
 
-  let { domain }: Props = $props();
+  const { domain }: Props = $props();
 
   const date = new Date();
 
@@ -20,7 +20,9 @@
   let disabled = $state(false);
   let p = $state({
     name: `invite-${date.getTime() % 10000000000}`,
-    domain_id: domain.id,
+    get domain_id() {
+      return domain.id;
+    },
   });
 
   // ---------------------------------------------------------------------------
