f6655817dd57c0abd12163ce66eb886b20592743
emrah <emrah.eryilmaz@conceptboard.com>
2026-03-16 23:17:35 +0300

    refactor(eb-app-api): sync adm/cronjob.ts

--- machines/eb-app-api/home/api/galaxy/lib/adm/cronjob.ts
+++ machines/eb-app-api/home/api/galaxy/lib/adm/cronjob.ts
@@ -1,16 +1,23 @@
 import { mailMeetingSession } from "../common/mail.ts";
 import { listMeetingSessionForReminder } from "../database/meeting-session.ts";
+import { Timers } from "./types.ts";
 
 // -----------------------------------------------------------------------------
 // Run every 30 seconds
 // -----------------------------------------------------------------------------
 async function remindMeetingSession(
+  t: Timers,
+  signal: AbortSignal,
   lastCheckTime = "2024-10-01T00:00:00.000Z",
 ) {
+  if (signal.aborted) return;
+
   try {
     const rows = await listMeetingSessionForReminder(lastCheckTime);
 
     for (const row of rows) {
+      if (signal.aborted) break;
+
       // Dont care failed mail.
       // Wait for each mail before processing the next one.
       // For now, I dont want to send all mails at the same second. One by one.
@@ -20,16 +27,21 @@ async function remindMeetingSession(
     }
   } catch (e) {
     console.log(e);
-  } finally {
-    setTimeout(() => remindMeetingSession(lastCheckTime), 30 * 1000);
   }
+
+  if (signal.aborted) return;
+
+  t.cronjobRemindMeetingSession = setTimeout(
+    () => remindMeetingSession(t, signal, lastCheckTime),
+    30 * 1000,
+  );
 }
 
 // -----------------------------------------------------------------------------
-export default function () {
+export default function (t: Timers, signal: AbortSignal) {
   console.log("cronjob is started");
 
   // dont wait for async functions
   // each function has its own cycle
-  remindMeetingSession();
+  remindMeetingSession(t, signal);
 }
