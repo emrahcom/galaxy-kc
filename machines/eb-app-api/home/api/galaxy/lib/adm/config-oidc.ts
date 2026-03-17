5ca98e691f84c0c54429eabe372a6d1f919fa2d2
emrah <emrah.eryilmaz@conceptboard.com>
2026-03-17 16:03:11 +0300

    refactor(eb-app-api): additional fields for the oidc config endpoint

--- machines/eb-app-api/home/api/galaxy/lib/adm/config-oidc.ts
+++ machines/eb-app-api/home/api/galaxy/lib/adm/config-oidc.ts
@@ -1,14 +1,17 @@
 import { ok } from "../http/response.ts";
 import { getAuthEndpoint, getLogoutEndpoint } from "../common/oidc.ts";
 import { CONTACT_EMAIL, GALAXY_FQDN } from "../../config.ts";
+import { OIDC_CLIENT_ID, OIDC_SCOPES } from "../../config.oidc.ts";
 
 // -----------------------------------------------------------------------------
 export default async function (): Promise<Response> {
   const config = [{
     contact_email: CONTACT_EMAIL,
     galaxy_fqdn: GALAXY_FQDN,
-    auth_endpoint: await getAuthEndpoint(),
-    logout_endpoint: await getLogoutEndpoint(),
+    oidc_auth_endpoint: await getAuthEndpoint(),
+    oidc_client_id: OIDC_CLIENT_ID,
+    oidc_logout_endpoint: await getLogoutEndpoint(),
+    oidc_scopes: OIDC_SCOPES,
   }];
 
   return ok(JSON.stringify(config));
